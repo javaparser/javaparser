d8df538374e64ed3fd88e9a28b15c6e9b93a8f6b
matozoid <lol@meuk.info>
2012-01-02 23:22:24 +0100

    * Add TODO

--- src/main/java/japa/parser/ast/body/ClassOrInterfaceDeclaration.java
+++ src/main/java/japa/parser/ast/body/ClassOrInterfaceDeclaration.java
@@ -34,77 +34,83 @@ import java.util.List;
  */
 public final class ClassOrInterfaceDeclaration extends TypeDeclaration {
 
-    private boolean interface_;
+	private boolean interface_;
 
-    private List<TypeParameter> typeParameters;
+	private List<TypeParameter> typeParameters;
 
-    private List<ClassOrInterfaceType> extendsList;
+	// TODO why is this a list when we only have single inheritance?
+	private List<ClassOrInterfaceType> extendsList;
 
-    private List<ClassOrInterfaceType> implementsList;
+	private List<ClassOrInterfaceType> implementsList;
 
-    public ClassOrInterfaceDeclaration() {
-    }
+	public ClassOrInterfaceDeclaration() {
+	}
 
-    public ClassOrInterfaceDeclaration(int modifiers, boolean isInterface, String name) {
-        super(modifiers, name);
-        this.interface_ = isInterface;
-    }
+	public ClassOrInterfaceDeclaration(final int modifiers, final boolean isInterface, final String name) {
+		super(modifiers, name);
+		this.interface_ = isInterface;
+	}
 
-    public ClassOrInterfaceDeclaration(JavadocComment javaDoc, int modifiers, List<AnnotationExpr> annotations, boolean isInterface, String name, List<TypeParameter> typeParameters, List<ClassOrInterfaceType> extendsList, List<ClassOrInterfaceType> implementsList, List<BodyDeclaration> members) {
-        super(annotations, javaDoc, modifiers, name, members);
-        this.interface_ = isInterface;
-        this.typeParameters = typeParameters;
-        this.extendsList = extendsList;
-        this.implementsList = implementsList;
-    }
+	public ClassOrInterfaceDeclaration(final JavadocComment javaDoc, final int modifiers,
+			final List<AnnotationExpr> annotations, final boolean isInterface, final String name,
+			final List<TypeParameter> typeParameters, final List<ClassOrInterfaceType> extendsList,
+			final List<ClassOrInterfaceType> implementsList, final List<BodyDeclaration> members) {
+		super(annotations, javaDoc, modifiers, name, members);
+		this.interface_ = isInterface;
+		this.typeParameters = typeParameters;
+		this.extendsList = extendsList;
+		this.implementsList = implementsList;
+	}
 
-    public ClassOrInterfaceDeclaration(int beginLine, int beginColumn, int endLine, int endColumn, JavadocComment javaDoc, int modifiers, List<AnnotationExpr> annotations, boolean isInterface, String name, List<TypeParameter> typeParameters, List<ClassOrInterfaceType> extendsList, List<ClassOrInterfaceType> implementsList, List<BodyDeclaration> members) {
-        super(beginLine, beginColumn, endLine, endColumn, annotations, javaDoc, modifiers, name, members);
-        this.interface_ = isInterface;
-        this.typeParameters = typeParameters;
-        this.extendsList = extendsList;
-        this.implementsList = implementsList;
-    }
+	public ClassOrInterfaceDeclaration(final int beginLine, final int beginColumn, final int endLine,
+			final int endColumn, final JavadocComment javaDoc, final int modifiers,
+			final List<AnnotationExpr> annotations, final boolean isInterface, final String name,
+			final List<TypeParameter> typeParameters, final List<ClassOrInterfaceType> extendsList,
+			final List<ClassOrInterfaceType> implementsList, final List<BodyDeclaration> members) {
+		super(beginLine, beginColumn, endLine, endColumn, annotations, javaDoc, modifiers, name, members);
+		this.interface_ = isInterface;
+		this.typeParameters = typeParameters;
+		this.extendsList = extendsList;
+		this.implementsList = implementsList;
+	}
 
-    @Override
-    public <R, A> R accept(GenericVisitor<R, A> v, A arg) {
-        return v.visit(this, arg);
-    }
+	@Override public <R, A> R accept(final GenericVisitor<R, A> v, final A arg) {
+		return v.visit(this, arg);
+	}
 
-    @Override
-    public <A> void accept(VoidVisitor<A> v, A arg) {
-        v.visit(this, arg);
-    }
+	@Override public <A> void accept(final VoidVisitor<A> v, final A arg) {
+		v.visit(this, arg);
+	}
 
-    public List<ClassOrInterfaceType> getExtends() {
-        return extendsList;
-    }
+	public List<ClassOrInterfaceType> getExtends() {
+		return extendsList;
+	}
 
-    public List<ClassOrInterfaceType> getImplements() {
-        return implementsList;
-    }
+	public List<ClassOrInterfaceType> getImplements() {
+		return implementsList;
+	}
 
-    public List<TypeParameter> getTypeParameters() {
-        return typeParameters;
-    }
+	public List<TypeParameter> getTypeParameters() {
+		return typeParameters;
+	}
 
-    public boolean isInterface() {
-        return interface_;
-    }
+	public boolean isInterface() {
+		return interface_;
+	}
 
-    public void setExtends(List<ClassOrInterfaceType> extendsList) {
-        this.extendsList = extendsList;
-    }
+	public void setExtends(final List<ClassOrInterfaceType> extendsList) {
+		this.extendsList = extendsList;
+	}
 
-    public void setImplements(List<ClassOrInterfaceType> implementsList) {
-        this.implementsList = implementsList;
-    }
+	public void setImplements(final List<ClassOrInterfaceType> implementsList) {
+		this.implementsList = implementsList;
+	}
 
-    public void setInterface(boolean interface_) {
-        this.interface_ = interface_;
-    }
+	public void setInterface(final boolean interface_) {
+		this.interface_ = interface_;
+	}
 
-    public void setTypeParameters(List<TypeParameter> typeParameters) {
-        this.typeParameters = typeParameters;
-    }
+	public void setTypeParameters(final List<TypeParameter> typeParameters) {
+		this.typeParameters = typeParameters;
+	}
 }
