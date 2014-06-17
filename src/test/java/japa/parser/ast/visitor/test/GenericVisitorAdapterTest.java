151452356070f3053c18c32782ce5d68978968fa
sebastiankirsch <Sebastian.Kirsch@gmail.com>
2014-06-17 15:05:25 +0200

    added tests for #38

---
+++ src/test/java/japa/parser/ast/visitor/test/GenericVisitorAdapterTest.java
@@ -0,0 +1,42 @@
+package japa.parser.ast.visitor.test;
+
+import japa.parser.ast.body.VariableDeclaratorId;
+import japa.parser.ast.stmt.BlockStmt;
+import japa.parser.ast.stmt.TryStmt;
+import japa.parser.ast.visitor.GenericVisitorAdapter;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public final class GenericVisitorAdapterTest extends VisitorAdapterTest {
+
+    @Test
+    public void visitsResourcesOfTryStmt() {
+        final String resourceName = "closeable";
+        TryStmt tryStmt = givenTryStmtWithResource(resourceName);
+
+        GenericVisitorAdapter<String, Void> objectUnderTest = new GenericVisitorAdapter<String, Void>() {
+            @Override
+            public String visit(VariableDeclaratorId n, Void arg) {
+                return n.getName();
+            }
+        };
+
+        String result = objectUnderTest.visit(tryStmt, null);
+
+        assertThat("Should have visited the TryStmt's resources!", result, is(equalTo(resourceName)));
+    }
+
+    @Test
+    public void handlesNullResourcesOfTryStmtGracefully() {
+        TryStmt tryStmt = new TryStmt(1, 1, 2, 80, null, new BlockStmt(), null, null);
+
+        GenericVisitorAdapter<Void, Void> objectUnderTest = new GenericVisitorAdapter<Void, Void>() {
+        };
+
+        objectUnderTest.visit(tryStmt, null);
+    }
+
+}
