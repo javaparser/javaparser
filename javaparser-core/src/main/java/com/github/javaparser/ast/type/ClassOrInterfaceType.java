20fcf8870b6e1272abdae45320aad28f1f98fb14
Federico Tomassetti <federico@tomassetti.me>
2015-02-18 15:45:31 +0000

    Adding missed Node subclasses with getName method

--- javaparser-core/src/main/java/com/github/javaparser/ast/type/ClassOrInterfaceType.java
+++ javaparser-core/src/main/java/com/github/javaparser/ast/type/ClassOrInterfaceType.java
@@ -21,6 +21,7 @@
  */
 package com.github.javaparser.ast.type;
 
+import com.github.javaparser.ast.NamedNode;
 import com.github.javaparser.ast.visitor.GenericVisitor;
 import com.github.javaparser.ast.visitor.VoidVisitor;
 
@@ -29,7 +30,7 @@ import java.util.List;
 /**
  * @author Julio Vilmar Gesser
  */
-public final class ClassOrInterfaceType extends Type {
+public final class ClassOrInterfaceType extends Type implements NamedNode {
 
 	private ClassOrInterfaceType scope;
 
