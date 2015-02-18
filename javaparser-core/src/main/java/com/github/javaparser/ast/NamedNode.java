1d9f32bde49a7be22e15093e94f414ef9b4ac36d
Federico Tomassetti <federico@tomassetti.me>
2015-02-18 15:13:15 +0000

    Specifyinf @since and explainining the reason for this interface

--- javaparser-core/src/main/java/com/github/javaparser/ast/NamedNode.java
+++ javaparser-core/src/main/java/com/github/javaparser/ast/NamedNode.java
@@ -2,6 +2,10 @@ package com.github.javaparser.ast;
 
 /**
  * A node having a name.
+ *  
+ * The main reason for this interface is to permit users to manipulate homogeneously all nodes with a getName method.
+ * 
+ * @since 2.0.1 
  */
 public interface NamedNode {
     String getName();
