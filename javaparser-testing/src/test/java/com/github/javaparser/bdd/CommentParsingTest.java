6fe0b4dd520279567ebcaf53860b53c75588e1d9
Federico Tomassetti <federico@tomassetti.me>
2015-02-19 10:34:34 +0000

    First attempt at adopting jbehave-junit-runner

--- javaparser-testing/src/test/java/com/github/javaparser/bdd/CommentParsingTest.java
+++ javaparser-testing/src/test/java/com/github/javaparser/bdd/CommentParsingTest.java
@@ -1,23 +1,21 @@
 package com.github.javaparser.bdd;
 
-import com.github.javaparser.bdd.embedders.CommentParsingEmbedder;
-import org.jbehave.core.embedder.Embedder;
-import org.jbehave.core.io.StoryFinder;
-import org.junit.Test;
+import com.github.javaparser.bdd.steps.CommentParsingSteps;
+import de.codecentric.jbehave.junit.monitoring.JUnitReportingRunner;
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.InstanceStepsFactory;
+import org.junit.runner.RunWith;
 
-import java.util.List;
+@RunWith(JUnitReportingRunner.class)
+public class CommentParsingTest extends BasicJBehaveTest {
 
-import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
-
-public class CommentParsingTest {
-
-    @Test
-    public void run() throws Throwable {
-        // Embedder defines the configuration and candidate steps
-        Embedder embedder = new CommentParsingEmbedder();
-        List<String> storyPaths = new StoryFinder().findPaths(codeLocationFromClass(this.getClass()), "**/bdd/comment*.story", "");
-        embedder.runStoriesAsPaths(storyPaths);
+    @Override
+    public InjectableStepsFactory stepsFactory() {
+        return new InstanceStepsFactory(configuration(), new CommentParsingSteps());
     }
 
+    public CommentParsingTest() {
+        super("**/bdd/comment*.story");
+    }
 }
 
--- javaparser-testing/src/test/java/com/github/javaparser/bdd/ParsingTest.java
+++ javaparser-testing/src/test/java/com/github/javaparser/bdd/ParsingTest.java
@@ -1,20 +1,30 @@
 package com.github.javaparser.bdd;
 
-import com.github.javaparser.bdd.embedders.ParsingEmbedder;
-import org.jbehave.core.embedder.Embedder;
-import org.jbehave.core.io.StoryFinder;
-import org.junit.Test;
+import com.github.javaparser.bdd.steps.ParsingSteps;
+import com.github.javaparser.bdd.steps.SharedSteps;
+import de.codecentric.jbehave.junit.monitoring.JUnitReportingRunner;
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.InstanceStepsFactory;
+import org.junit.runner.RunWith;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
+@RunWith(JUnitReportingRunner.class)
+public class ParsingTest extends BasicJBehaveTest {
+    
+    @Override
+    public InjectableStepsFactory stepsFactory() {
+        Map<String, Object> state = new HashMap<String, Object>();
 
-public class ParsingTest {
-
-    @Test
-    public void run() throws Throwable {
-        Embedder embedder = new ParsingEmbedder();
-        List<String> storyPaths = new StoryFinder().findPaths(codeLocationFromClass(this.getClass()), "**/bdd/parsing*.story", "");
-        embedder.runStoriesAsPaths(storyPaths);
+        return new InstanceStepsFactory(configuration(),
+                new SharedSteps(state),
+                new ParsingSteps(state));
+    }
+    
+    public ParsingTest() {
+        super("**/bdd/parsing*.story");
     }
 }
+
+
