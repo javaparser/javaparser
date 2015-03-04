e2d2a645452cea424134263597fd83bf67323282
Didier Villevalois <ptitjes@free.fr>
2015-03-04 18:44:24 +0100

    [Lex] Post-process token to create Lexemes

--- javaparser-core/src/main/java/com/github/javaparser/parser/Parser.java
+++ javaparser-core/src/main/java/com/github/javaparser/parser/Parser.java
@@ -1,11 +1,13 @@
 package com.github.javaparser.parser;
 
+import com.github.javaparser.Position;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
 import com.github.javaparser.ast.body.BodyDeclaration;
 import com.github.javaparser.ast.body.ModifierSet;
 import com.github.javaparser.ast.expr.AnnotationExpr;
 import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.lexical.Lexeme;
 import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.stmt.Statement;
 
@@ -62,22 +64,46 @@ public abstract class Parser {
         return list;
     }
 
-    protected class Modifier {
+    protected class ModifiersAndAnnotations {
 
-        final int modifiers;
-        final List annotations;
-        final int beginLine;
-        final int beginColumn;
+        public final Lexeme first;
+        private int modifiers;
+        private List annotations;
 
-        public Modifier(int beginLine, int beginColumn, int modifiers, List annotations) {
-            this.beginLine = beginLine;
-            this.beginColumn = beginColumn;
-            this.modifiers = modifiers;
-            this.annotations = annotations;
+        public ModifiersAndAnnotations(Lexeme first) {
+            this.first = first;
+        }
+
+        void addModifier(int mod, Token token) throws ParseException {
+            if ((ModifierSet.hasModifier(modifiers, mod))) {
+                // TODO We should not stop the parsing for such an error
+                // This defeats the purpose of recognizing all mods in one production
+                throwParseException(token, "Duplicated modifier");
+            }
+            modifiers = ModifierSet.addModifier(modifiers, mod);
+        }
+
+        void addAnnotation(AnnotationExpr annotation) {
+            if (annotations == null) {
+                annotations = new LinkedList();
+            }
+            annotations.add(annotation);
+        }
+
+        public List annotations() {
+            return annotations;
+        }
+
+        public int modifiers() {
+            return modifiers;
         }
     }
 
-    public int addModifier(int modifiers, int mod, Token token) throws ParseException {
+    protected ModifiersAndAnnotations newModifiersAndAnnotations() {
+        return new ModifiersAndAnnotations(next());
+    }
+
+    protected int addModifier(int modifiers, int mod, Token token) throws ParseException {
         if ((ModifierSet.hasModifier(modifiers, mod))) {
             throwParseException(token, "Duplicated modifier");
         }
@@ -85,4 +111,42 @@ public abstract class Parser {
     }
 
     protected abstract void throwParseException(Token token, String message) throws ParseException;
+
+    void postProcessToken(Token token) {
+        lastLexeme = buildLexemeChain(token, null);
+        if (firstLexeme == null) firstLexeme = lastLexeme;
+    }
+
+    private Lexeme firstLexeme;
+    private Lexeme lastLexeme;
+
+    private Lexeme buildLexemeChain(Token token, Lexeme next) {
+        Lexeme current = TokenLexemeConversion.instantiate(token.kind, token.image);
+        token.lexeme = current;
+
+        current.setBegin(new Position(token.beginLine, token.beginColumn));
+        current.setEnd(new Position(token.endLine, token.endColumn));
+
+        Lexeme previous;
+        if (token.specialToken != null) {
+            previous = buildLexemeChain(token.specialToken, current);
+        } else {
+            previous = lastLexeme;
+        }
+
+        current.setPrevious(previous);
+        if (previous != null) previous.setNext(current);
+
+        return current;
+    }
+
+    protected abstract Token getToken(int index);
+
+    protected Lexeme next() {
+        return getToken(1).lexeme;
+    }
+
+    protected Lexeme previous() {
+        return getToken(0).lexeme;
+    }
 }
