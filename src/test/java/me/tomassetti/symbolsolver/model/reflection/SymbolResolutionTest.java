4ebd1c4b83764a565249aa84ce1dc07050129765
Federico Tomassetti <federico@tomassetti.me>
2015-08-26 16:27:02 +0200

    add test for the failure on the conditional operator

--- src/test/java/me/tomassetti/symbolsolver/model/reflection/SymbolResolutionTest.java
+++ src/test/java/me/tomassetti/symbolsolver/model/reflection/SymbolResolutionTest.java
@@ -3,7 +3,11 @@ package me.tomassetti.symbolsolver.model.reflection;
 import com.github.javaparser.ParseException;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.stmt.ReturnStmt;
 import me.tomassetti.symbolsolver.JavaParserFacade;
 import me.tomassetti.symbolsolver.javaparser.Navigator;
 import me.tomassetti.symbolsolver.model.AbstractTest;
@@ -19,6 +23,8 @@ import me.tomassetti.symbolsolver.model.typesolvers.JreTypeSolver;
 import me.tomassetti.symbolsolver.model.usages.TypeUsage;
 import org.junit.Test;
 
+import java.lang.reflect.Method;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -26,7 +32,7 @@ import static org.junit.Assert.assertEquals;
  */
 public class SymbolResolutionTest extends AbstractTest {
 
-    /*@Test
+    @Test
     public void getTypeOfField() throws ParseException {
         CompilationUnit cu = parseSample("ReflectionFieldOfItself");
         ClassOrInterfaceDeclaration clazz = Navigator.demandClass(cu, "MyClass");
@@ -35,7 +41,7 @@ public class SymbolResolutionTest extends AbstractTest {
         TypeSolver typeSolver = new JreTypeSolver();
         TypeUsage ref = JavaParserFacade.get(typeSolver).getType(field);
         assertEquals("int", ref.getTypeName());
-    }*/
+    }
 
     @Test
     public void getTypeOfFieldAccess() throws ParseException {
@@ -46,6 +52,31 @@ public class SymbolResolutionTest extends AbstractTest {
         TypeSolver typeSolver = new JreTypeSolver();
         TypeUsage ref = JavaParserFacade.get(typeSolver).getType(field.getInit());
         assertEquals("int", ref.getTypeName());
+    }
+
+    @Test
+    public void conditionalExpressionExample() throws ParseException {
+        CompilationUnit cu = parseSample("JreConditionalExpression");
+        ClassOrInterfaceDeclaration clazz = Navigator.demandClass(cu, "MyClass");
+        MethodDeclaration method = Navigator.demandMethod(clazz, "foo1");
+        ReturnStmt returnStmt = Navigator.findReturnStmt(method);
+        Expression expression = returnStmt.getExpr();
+
+        TypeSolver typeSolver = new JreTypeSolver();
+        TypeUsage ref = JavaParserFacade.get(typeSolver).getType(expression);
+        assertEquals("java.lang.String", ref.getTypeName());
+    }
+
+    @Test
+    public void conditionalExpressionExampleFollowUp1() throws ParseException {
+        CompilationUnit cu = parseSample("JreConditionalExpression");
+        ClassOrInterfaceDeclaration clazz = Navigator.demandClass(cu, "MyClass");
+        MethodDeclaration method = Navigator.demandMethod(clazz, "foo1");
+        MethodCallExpr expression = Navigator.findMethodCall(method, "hasNext");
+
+        TypeSolver typeSolver = new JreTypeSolver();
+        TypeUsage ref = JavaParserFacade.get(typeSolver).getType(expression);
+        assertEquals("java.lang.String", ref.getTypeName());
     }
 
 }
