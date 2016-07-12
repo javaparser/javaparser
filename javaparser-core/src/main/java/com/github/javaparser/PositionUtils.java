33299776265b9acd160a5196cb9306fd9a7085ec
matozoid <hexagonaal@gmail.com>
2016-07-12 23:26:01 +0200

    Reminder for possible bug

--- javaparser-core/src/main/java/com/github/javaparser/PositionUtils.java
+++ javaparser-core/src/main/java/com/github/javaparser/PositionUtils.java
@@ -132,6 +132,7 @@ public final class PositionUtils {
             if (bl>contained.getBeginLine()) return false;
             if (bl==contained.getBeginLine() && bc>contained.getBeginColumn()) return false;
             if (container.getEndLine()<contained.getEndLine()) return false;
+            // TODO < or <= ?
             return !(container.getEndLine() == contained.getEndLine() && container.getEndColumn() < contained.getEndColumn());
         }
         return true;
