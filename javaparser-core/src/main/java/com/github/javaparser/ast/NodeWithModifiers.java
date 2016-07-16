9eae286ac348dbab1998a8c79901d190436f474d
Maximilien CRUZ <max@rockpalm.it>
2016-07-16 13:47:59 +0200

    Added all default modifiers helpers as @ftomassetti proposed in #57

--- javaparser-core/src/main/java/com/github/javaparser/ast/NodeWithModifiers.java
+++ javaparser-core/src/main/java/com/github/javaparser/ast/NodeWithModifiers.java
@@ -6,11 +6,55 @@ import com.github.javaparser.ast.body.ModifierSet;
  * A Node with Modifiers.
  */
 public interface NodeWithModifiers {
-	/**
-	 * Return the modifiers of this variable declaration.
-	 *
-	 * @see ModifierSet
-	 * @return modifiers
-	 */
-	int getModifiers();
+    /**
+     * Return the modifiers of this variable declaration.
+     *
+     * @see ModifierSet
+     * @return modifiers
+     */
+    int getModifiers();
+
+    default boolean isStatic() {
+        return ModifierSet.isStatic(getModifiers());
+    }
+
+    default boolean isAbstract() {
+        return ModifierSet.isAbstract(getModifiers());
+    }
+
+    default boolean isFinal() {
+        return ModifierSet.isFinal(getModifiers());
+    }
+
+    default boolean isNative() {
+        return ModifierSet.isNative(getModifiers());
+    }
+
+    default boolean isPrivate() {
+        return ModifierSet.isPrivate(getModifiers());
+    }
+
+    default boolean isProtected() {
+        return ModifierSet.isProtected(getModifiers());
+    }
+
+    default boolean isPublic() {
+        return ModifierSet.isPublic(getModifiers());
+    }
+
+    default boolean isStrictfp() {
+        return ModifierSet.isStrictfp(getModifiers());
+    }
+
+    default boolean isSynchronized() {
+        return ModifierSet.isSynchronized(getModifiers());
+    }
+
+    default boolean isTransient() {
+        return ModifierSet.isTransient(getModifiers());
+    }
+
+    default boolean isVolatile() {
+        return ModifierSet.isVolatile(getModifiers());
+    }
 }
