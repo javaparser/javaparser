9f107a30ea7a92c5d24d4c2641803a1783dd20a6
Maximilien Cruz <maximilien.cruz@gmail.com>
2016-07-18 16:33:41 +0200

    equals must be used now, all tests pass ! :)

--- javaparser-core/src/main/java/com/github/javaparser/ast/visitor/EqualsVisitor.java
+++ javaparser-core/src/main/java/com/github/javaparser/ast/visitor/EqualsVisitor.java
@@ -21,10 +21,10 @@
  
 package com.github.javaparser.ast.visitor;
 
-import com.github.javaparser.ast.comments.BlockComment;
+import java.util.List;
+
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
-import com.github.javaparser.ast.comments.LineComment;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.PackageDeclaration;
 import com.github.javaparser.ast.TypeParameter;
@@ -39,13 +39,50 @@ import com.github.javaparser.ast.body.EnumConstantDeclaration;
 import com.github.javaparser.ast.body.EnumDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.InitializerDeclaration;
-import com.github.javaparser.ast.comments.JavadocComment;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.MultiTypeParameter;
 import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.body.VariableDeclaratorId;
-import com.github.javaparser.ast.expr.*;
+import com.github.javaparser.ast.comments.BlockComment;
+import com.github.javaparser.ast.comments.JavadocComment;
+import com.github.javaparser.ast.comments.LineComment;
+import com.github.javaparser.ast.expr.AnnotationExpr;
+import com.github.javaparser.ast.expr.ArrayAccessExpr;
+import com.github.javaparser.ast.expr.ArrayCreationExpr;
+import com.github.javaparser.ast.expr.ArrayInitializerExpr;
+import com.github.javaparser.ast.expr.AssignExpr;
+import com.github.javaparser.ast.expr.BinaryExpr;
+import com.github.javaparser.ast.expr.BooleanLiteralExpr;
+import com.github.javaparser.ast.expr.CastExpr;
+import com.github.javaparser.ast.expr.CharLiteralExpr;
+import com.github.javaparser.ast.expr.ClassExpr;
+import com.github.javaparser.ast.expr.ConditionalExpr;
+import com.github.javaparser.ast.expr.DoubleLiteralExpr;
+import com.github.javaparser.ast.expr.EnclosedExpr;
+import com.github.javaparser.ast.expr.FieldAccessExpr;
+import com.github.javaparser.ast.expr.InstanceOfExpr;
+import com.github.javaparser.ast.expr.IntegerLiteralExpr;
+import com.github.javaparser.ast.expr.IntegerLiteralMinValueExpr;
+import com.github.javaparser.ast.expr.LambdaExpr;
+import com.github.javaparser.ast.expr.LongLiteralExpr;
+import com.github.javaparser.ast.expr.LongLiteralMinValueExpr;
+import com.github.javaparser.ast.expr.MarkerAnnotationExpr;
+import com.github.javaparser.ast.expr.MemberValuePair;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.MethodReferenceExpr;
+import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.expr.NormalAnnotationExpr;
+import com.github.javaparser.ast.expr.NullLiteralExpr;
+import com.github.javaparser.ast.expr.ObjectCreationExpr;
+import com.github.javaparser.ast.expr.QualifiedNameExpr;
+import com.github.javaparser.ast.expr.SingleMemberAnnotationExpr;
+import com.github.javaparser.ast.expr.StringLiteralExpr;
+import com.github.javaparser.ast.expr.SuperExpr;
+import com.github.javaparser.ast.expr.ThisExpr;
+import com.github.javaparser.ast.expr.TypeExpr;
+import com.github.javaparser.ast.expr.UnaryExpr;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
 import com.github.javaparser.ast.stmt.AssertStmt;
 import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.stmt.BreakStmt;
@@ -67,9 +104,14 @@ import com.github.javaparser.ast.stmt.ThrowStmt;
 import com.github.javaparser.ast.stmt.TryStmt;
 import com.github.javaparser.ast.stmt.TypeDeclarationStmt;
 import com.github.javaparser.ast.stmt.WhileStmt;
-import com.github.javaparser.ast.type.*;
-
-import java.util.List;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.type.IntersectionType;
+import com.github.javaparser.ast.type.PrimitiveType;
+import com.github.javaparser.ast.type.ReferenceType;
+import com.github.javaparser.ast.type.UnionType;
+import com.github.javaparser.ast.type.UnknownType;
+import com.github.javaparser.ast.type.VoidType;
+import com.github.javaparser.ast.type.WildcardType;
 
 /**
  * @author Julio Vilmar Gesser
@@ -235,7 +277,7 @@ public class EqualsVisitor implements GenericVisitor<Boolean, Node> {
 
 		// javadoc are checked at CompilationUnit
 
-		if (n1.getModifiers() != n2.getModifiers()) {
+        if (!n1.getModifiers().equals(n2.getModifiers())) {
 			return false;
 		}
 
@@ -275,7 +317,7 @@ public class EqualsVisitor implements GenericVisitor<Boolean, Node> {
 
 		// javadoc are checked at CompilationUnit
 
-		if (n1.getModifiers() != n2.getModifiers()) {
+        if (!n1.getModifiers().equals(n2.getModifiers())) {
 			return false;
 		}
 
@@ -335,7 +377,7 @@ public class EqualsVisitor implements GenericVisitor<Boolean, Node> {
 
 		// javadoc are checked at CompilationUnit
 
-		if (n1.getModifiers() != n2.getModifiers()) {
+        if (!n1.getModifiers().equals(n2.getModifiers())) {
 			return false;
 		}
 
@@ -359,7 +401,7 @@ public class EqualsVisitor implements GenericVisitor<Boolean, Node> {
 
 		// javadoc are checked at CompilationUnit
 
-		if (n1.getModifiers() != n2.getModifiers()) {
+        if (!n1.getModifiers().equals(n2.getModifiers())) {
 			return false;
 		}
 
@@ -387,7 +429,7 @@ public class EqualsVisitor implements GenericVisitor<Boolean, Node> {
 
 		// javadoc are checked at CompilationUnit
 
-		if (n1.getModifiers() != n2.getModifiers()) {
+        if (!n1.getModifiers().equals(n2.getModifiers())) {
 			return false;
 		}
 
@@ -439,7 +481,7 @@ public class EqualsVisitor implements GenericVisitor<Boolean, Node> {
 
 		// javadoc are checked at CompilationUnit
 
-		if (n1.getModifiers() != n2.getModifiers()) {
+        if (!n1.getModifiers().equals(n2.getModifiers())) {
 			return false;
 		}
 
@@ -475,7 +517,7 @@ public class EqualsVisitor implements GenericVisitor<Boolean, Node> {
 
 		// javadoc are checked at CompilationUnit
 
-		if (n1.getModifiers() != n2.getModifiers()) {
+        if (!n1.getModifiers().equals(n2.getModifiers())) {
 			return false;
 		}
 
@@ -535,7 +577,7 @@ public class EqualsVisitor implements GenericVisitor<Boolean, Node> {
 	protected Boolean visit(final BaseParameter n1, final Node arg) {
 		final BaseParameter n2 = (BaseParameter) arg;
 
-		if (n1.getModifiers() != n2.getModifiers()) {
+        if (!n1.getModifiers().equals(n2.getModifiers())) {
 			return false;
 		}
 
@@ -1107,7 +1149,7 @@ public class EqualsVisitor implements GenericVisitor<Boolean, Node> {
 	@Override public Boolean visit(final VariableDeclarationExpr n1, final Node arg) {
 		final VariableDeclarationExpr n2 = (VariableDeclarationExpr) arg;
 
-		if (n1.getModifiers() != n2.getModifiers()) {
+        if (!n1.getModifiers().equals(n2.getModifiers())) {
 			return false;
 		}
 
