8b058a3c4cc58731755bdc1975930d53b23d4162
malte_langkabel <malte.langkabel@gmail.com>
2016-09-23 20:06:06 +0200

    Changed author of LambdaExprContextTest.java :)

--- java-symbol-solver-core/src/test/java/me/tomassetti/symbolsolver/resolution/javaparser/contexts/LambdaExprContextTest.java
+++ java-symbol-solver-core/src/test/java/me/tomassetti/symbolsolver/resolution/javaparser/contexts/LambdaExprContextTest.java
@@ -27,7 +27,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- * @author Federico Tomassetti
+ * @author Malte Langkabel
  */
 public class LambdaExprContextTest extends AbstractTest {
 
