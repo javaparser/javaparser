742b54996297defda681773ebc573cd5fb98582e
zero33542 <kaichih1013@gmail.com>
2016-09-25 14:59:51 -0500

    Add files via upload

---
+++ javaparser-core/src/main/java/com/github/javaparser/ast/visitor/TreeVisitor.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2007-2010 Júlio Vilmar Gesser.
+ * Copyright (C) 2011, 2013-2016 The JavaParser Team.
+ *
+ * This file is part of JavaParser.
+ * 
+ * JavaParser can be used either under the terms of
+ * a) the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * b) the terms of the Apache License 
+ *
+ * You should have received a copy of both licenses in LICENCE.LGPL and
+ * LICENCE.APACHE. Please refer to those files for details.
+ *
+ * JavaParser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ */
+
+package com.github.javaparser.ast.visitor;
+
+import com.github.javaparser.ast.Node;
+import java.util.LinkdedList;
+import java.util.Queue;
+
+/**
+ * Iterate over all the nodes in (a part of) the AST.
+ */
+public abstract class TreeVisitor {
+
+	/**
+	 * https://en.wikipedia.org/wiki/Depth-first_search
+	 *
+	 * @param node the start node, and the first one that is passed to process(node).
+	 */
+	public void visitDepthFirst(Node node) {
+		process(node);
+		for (Node child : node.getChildrenNodes()) {
+			visitDepthFirst(child);
+		}
+	}
+	
+	/**
+	* https://en.wikipedia.org/wiki/Breadth-first_search
+	*
+	* @param node the start node, and the first one that is passed to process(node).
+	*/
+	public void visitBreadthFirst(Node node) {
+		Queue<Node> queue = new LinkedList<Node>();
+		queue.offer(node);
+		while (queue.size() > 0) {
+			Node head = queue.peek();
+			for (Node child : head.getChildrenNodes()) {
+				queue.offer(child);
+			}
+			process(queue.poll());
+		}
+	}
+
+	public abstract void process(Node node);
+}
