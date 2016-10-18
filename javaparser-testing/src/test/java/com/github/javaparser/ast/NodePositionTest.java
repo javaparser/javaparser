197ee0b76e55c6db8b4d3ba212fc6c67716c5e02
Federico Tomassetti <federico@tomassetti.me>
2016-10-18 20:52:14 +0200

    issue506: explicitly exclude the ArrayBracketPair from the nodes which must have a position

--- javaparser-testing/src/test/java/com/github/javaparser/ast/NodePositionTest.java
+++ javaparser-testing/src/test/java/com/github/javaparser/ast/NodePositionTest.java
@@ -11,6 +11,8 @@ import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
 
+import static org.junit.Assert.assertTrue;
+
 public class NodePositionTest {
 
     private List<Node> getAllNodes(Node node) {
@@ -24,11 +26,13 @@ public class NodePositionTest {
     public void issue506() throws IOException {
         InputStream is = this.getClass().getResourceAsStream("/com/github/javaparser/SourcesHelperOldVersion.java.txt");
         ParseResult<CompilationUnit> res = new JavaParser().parse(ParseStart.COMPILATION_UNIT, new StreamProvider(is));
+        assertTrue(res.getProblems().isEmpty());
 
         CompilationUnit cu = res.getResult().get();
         getAllNodes(cu).forEach(n -> {
-            if (n.getBegin().line == 0) {
-                throw new IllegalArgumentException("There should be no node at line 0");
+            if (n.getBegin().line == 0 && !n.toString().isEmpty() && !(n instanceof NodeList) && !(n instanceof ArrayBracketPair)) {
+                throw new IllegalArgumentException("There should be no node at line 0: " + n + " (class: "
+                        + n.getClass().getCanonicalName()+ ")");
             }
         });
     }
