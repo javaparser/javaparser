1b63f28d6ceb0beb99c8b0e841c2c62de7f57f6f
Federico Tomassetti <f.tomassetti@gmail.com>
2016-10-19 13:36:03 +0200

    issue506: some nodes have line zero (#507)
    * issue506: failing test proving issue 506
    * NodeList: comparing ranges should use equals and not ==
    * deriving the position of ArrayType from the position of the component type
    * issue506: explicitly exclude the ArrayBracketPair from the nodes which must have a position

---
+++ javaparser-testing/src/test/java/com/github/javaparser/ast/NodePositionTest.java
@@ -0,0 +1,39 @@
+package com.github.javaparser.ast;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ParseStart;
+import com.github.javaparser.StreamProvider;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class NodePositionTest {
+
+    private List<Node> getAllNodes(Node node) {
+        List<Node> nodes = new LinkedList<>();
+        nodes.add(node);
+        node.getChildrenNodes().forEach(c -> nodes.addAll(getAllNodes(c)));
+        return nodes;
+    }
+
+    @Test
+    public void issue506() throws IOException {
+        InputStream is = this.getClass().getResourceAsStream("/com/github/javaparser/SourcesHelperOldVersion.java.txt");
+        ParseResult<CompilationUnit> res = new JavaParser().parse(ParseStart.COMPILATION_UNIT, new StreamProvider(is));
+        assertTrue(res.getProblems().isEmpty());
+
+        CompilationUnit cu = res.getResult().get();
+        getAllNodes(cu).forEach(n -> {
+            if (n.getBegin().line == 0 && !n.toString().isEmpty() && !(n instanceof NodeList) && !(n instanceof ArrayBracketPair)) {
+                throw new IllegalArgumentException("There should be no node at line 0: " + n + " (class: "
+                        + n.getClass().getCanonicalName()+ ")");
+            }
+        });
+    }
+}
