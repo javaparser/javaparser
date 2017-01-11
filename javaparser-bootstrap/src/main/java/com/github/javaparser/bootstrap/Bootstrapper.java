8c42b2c4013fa95ebe3852ccecfc937580e023ff
Danny van Bruggen <hexagonaal@gmail.com>
2017-01-11 20:25:39 +0100

    Fix build

--- javaparser-bootstrap/src/main/java/com/github/javaparser/bootstrap/Bootstrapper.java
+++
@@ -1,9 +0,0 @@
-package com.github.javaparser.bootstrap;
-
-import java.io.IOException;
-
-public class Bootstrapper {
-    public static void main(String[] args) throws IOException {
-        VisitorGenerator.main(args);
-    }
-}
