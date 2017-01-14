8736064a0955788062faaf3c629f943db50b1b53
Federico Tomassetti <federico@tomassetti.me>
2017-01-14 16:48:36 +0100

    issue705: introducing ConcreteSyntaxModel.prettyPrint

--- javaparser-core/src/main/java/com/github/javaparser/printer/ConcreteSyntaxModel.java
+++ javaparser-core/src/main/java/com/github/javaparser/printer/ConcreteSyntaxModel.java
@@ -21,10 +21,15 @@
 
 package com.github.javaparser.printer;
 
+import com.github.javaparser.ASTParserConstants;
 import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.ClassExpr;
 import com.github.javaparser.ast.observer.ObservableProperty;
 
+import java.util.LinkedList;
 import java.util.List;
+
+import static com.github.javaparser.ast.observer.ObservableProperty.TYPE;
 
 /**
  * The Concrete Syntax Model for a single node type. It knows the syntax used to represent a certain element in Java
@@ -32,22 +37,42 @@ import java.util.List;
  */
 public class ConcreteSyntaxModel {
 
-    interface Element {
+    List<Element> elements;
 
+    interface Element {
+        void prettyPrint(Node node, SourcePrinter printer);
     }
 
-    class StringElement implements Element {
+    private static class StringElement implements Element {
         private int tokenType;
         private String content;
+
+        public StringElement(int tokenType, String content) {
+            this.tokenType = tokenType;
+            this.content = content;
+        }
     }
 
-    class ChildElement implements Element {
+    private static class ChildElement implements Element {
         private ObservableProperty property;
+
+        public ChildElement(ObservableProperty property) {
+            this.property = property;
+        }
     }
 
-    class ListElement implements Element {
+    private static class ListElement implements Element {
         private ObservableProperty property;
         private StringElement separator;
+    }
+
+    private static class CommentElement implements Element {
+        @Override
+        public void prettyPrint(Node node, SourcePrinter printer) {
+            if (node.hasComment()) {
+                genericPrettyPrint(node.getComment(), printer);
+            }
+        }
     }
 
     public List<Element> getElements() {
@@ -58,7 +83,61 @@ public class ConcreteSyntaxModel {
 
     }
 
+    public void prettyPrint(Node node, SourcePrinter printer) {
+        elements.forEach(e -> e.prettyPrint(node, printer));
+    }
+
+    private static class Builder {
+        List<Element> elements = new LinkedList<>();
+
+        Builder add(Element e) {
+            elements.add(e);
+            return this;
+        }
+
+        Builder comment() {
+            return add(new CommentElement());
+        }
+
+        Builder child(ObservableProperty property) {
+            return add(new ChildElement(property));
+        }
+
+        Builder string(int tokenType, String content) {
+            return add(new StringElement(tokenType, content));
+        }
+
+        Builder string(int tokenType) {
+            return string(tokenType, ASTParserConstants.tokenImage[tokenType]);
+        }
+
+        ConcreteSyntaxModel build() {
+            ConcreteSyntaxModel instance = new ConcreteSyntaxModel();
+            instance.elements = this.elements;
+            return instance;
+        }
+    }
+
+    public static void genericPrettyPrint(Node node, SourcePrinter printer) {
+        forClass(node.getClass()).prettyPrint(node, printer);
+    }
+
     public static ConcreteSyntaxModel forClass(Class<? extends Node> nodeClazz) {
+
+        if (nodeClazz.equals(ClassExpr.class)) {
+            return new Builder().comment().child(TYPE)
+                    .string(ASTParserConstants.DOT)
+                    .string(ASTParserConstants.CLASS)
+                    .build();
+        }
+
+//        @Override
+//        public void visit(final ClassExpr n, final Void arg) {
+//            printJavaComment(n.getComment(), arg);
+//            n.getType().accept(this, arg);
+//            printer.print(".class");
+//        }
+
         throw new UnsupportedOperationException();
     }
 
