e6a301b1485e6da8d919c1ec52f4949e9ebbcd45
Danny van Bruggen <hexagonaal@gmail.com>
2017-02-02 21:39:11 +0100

    Test typeless xml output

--- javaparser-testing/src/test/java/com/github/javaparser/ast/visitor/treestructure/TreeStructureXmlCallbackTest.java
+++ javaparser-testing/src/test/java/com/github/javaparser/ast/visitor/treestructure/TreeStructureXmlCallbackTest.java
@@ -4,11 +4,11 @@ import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.expr.Expression;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class TreeStructureXmlCallbackTest {
     @Test
-    public void test() {
+    public void testWithType() {
         StringBuilder stringBuilder = new StringBuilder();
         TreeStructureVisitor visitor = new TreeStructureVisitor(new TreeStructureXmlCallback(stringBuilder, true));
         Expression expression = JavaParser.parseExpression("1+1");
@@ -16,5 +16,16 @@ public class TreeStructureXmlCallbackTest {
         expression.accept(visitor, new Context());
 
         assertEquals("<root type='BinaryExpr' operator='PLUS'><left type='IntegerLiteralExpr' value='1'></left><right type='IntegerLiteralExpr' value='1'></right></root>", stringBuilder.toString());
+    }
+
+    @Test
+    public void testWithoutType() {
+        StringBuilder stringBuilder = new StringBuilder();
+        TreeStructureVisitor visitor = new TreeStructureVisitor(new TreeStructureXmlCallback(stringBuilder, false));
+        Expression expression = JavaParser.parseExpression("1+1");
+
+        expression.accept(visitor, new Context());
+
+        assertEquals("<root operator='PLUS'><left value='1'></left><right value='1'></right></root>", stringBuilder.toString());
     }
 }
