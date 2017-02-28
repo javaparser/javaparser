8bcb8538c3c913ddf6a43e656d5d663c7035db7e
Danny van Bruggen <hexagonaal@gmail.com>
2017-02-28 20:20:08 +0100

    Prepare to update the openjdk parsing tool

--- javaparser-testing/src/test/java/com/github/javaparser/manual/ParseDirectory.java
+++ javaparser-testing/src/test/java/com/github/javaparser/manual/ParseDirectory.java
@@ -2,10 +2,14 @@ package com.github.javaparser.manual;
 
 import com.github.javaparser.*;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.utils.CodeGenerationUtils;
+import com.github.javaparser.utils.SourceRoot;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static com.github.javaparser.ParseStart.COMPILATION_UNIT;
 import static com.github.javaparser.Providers.provider;
@@ -18,6 +22,8 @@ import static com.github.javaparser.Providers.provider;
  */
 public class ParseDirectory {
     public static void main(String[] args) throws IOException {
+        SourceRoot sourceRoot = new SourceRoot(CodeGenerationUtils.mavenModuleRoot());
+//        sourceRoot.
         JavaParser parser = new JavaParser();
         Files.find(new File("/home/danny/tmp/openjdk/").toPath(), 30, (path, basicFileAttributes) -> true).forEach(p -> {
             if (!Files.isDirectory(p) && p.toString().endsWith(".java")) {
