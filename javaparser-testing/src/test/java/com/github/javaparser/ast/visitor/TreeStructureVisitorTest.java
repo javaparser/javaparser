46620b8ac654a199b5415d6a94606842f79b5114
Ryan Beckett <beckett.ryan@gmail.com>
2017-03-02 06:46:17 -0500

    issue753: Added formatting framework tests

--- javaparser-testing/src/test/java/com/github/javaparser/ast/visitor/TreeStructureVisitorTest.java
+++ javaparser-testing/src/test/java/com/github/javaparser/ast/visitor/TreeStructureVisitorTest.java
@@ -1,21 +1,81 @@
 package com.github.javaparser.ast.visitor;
 
-import com.github.javaparser.JavaParser;
-import com.github.javaparser.ast.CompilationUnit;
 import org.junit.Test;
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.utils.format.Format;
+import com.github.javaparser.utils.format.FormatInstructions;
+import com.github.javaparser.utils.format.FormatInstructionsFactory;
+
+/**
+ * A set of tests verifying the output of baked-in formatting instructions.
+ *
+ * @see TreeStructureVisitorGenerator
+ * @see FormatInstructions
+ * @version 3.1.0
+ * @since 3.1.0
+ * @author Danny van Bruggen
+ * @author Ryan Beckett
+ *
+ */
 public class TreeStructureVisitorTest {
+
+    private static final String src = "package mypackage;\n" +
+            "\n" +
+            "public final class MyClass {\n" +
+            "\n" +
+            "    public void myMethod() {\n" +
+            "        if ((hello() == \"abc\" && foo() == \"def\" && 1 == 1) || (2 == 2)) {\n" +
+            "        }\n" +
+            "    }\n" +
+            "    public void myMethod2() {}\n" +
+            "    public void myMethod3() {}\n" +
+            "}";
+    private static final CompilationUnit cu = JavaParser.parse(src);
+
     @Test
-    public void dumpSomething() {
-        CompilationUnit parse = JavaParser.parse("package mypackage;\n" +
-                "\n" +
-                "public class MyClass {\n" +
-                "\n" +
-                "    public void myMethod() {\n" +
-                "        if (hello() == \"abc\" && foo() == \"def\") {\n" +
-                "        }\n" +
-                "    }\n" +
-                "}");
-        parse.accept(new TreeStructureVisitor(), 0);
+    public void dumpDefault() {
+        TreeStructureVisitor tsv = new TreeStructureVisitor();
+        cu.accept(tsv, 0);
+        System.out.println(tsv.getResult() + "\n----------------------");
     }
+
+    @Test
+    public void dumpDefaultWithNoIndent() {
+        TreeStructureVisitor tsv = new TreeStructureVisitor(false, true);
+        cu.accept(tsv, 0);
+        System.out.println(tsv.getResult() + "\n----------------------");
+    }
+
+    @Test
+    public void dumpDefaultWithNoNewline() {
+        TreeStructureVisitor tsv = new TreeStructureVisitor(true, false);
+        cu.accept(tsv, 0);
+        System.out.println(tsv.getResult() + "\n----------------------");
+    }
+
+    @Test
+    public void dumpDefaultWithNoIdentNorNewline() {
+        TreeStructureVisitor tsv = new TreeStructureVisitor(false, false);
+        cu.accept(tsv, 0);
+        System.out.println(tsv.getResult() + "\n----------------------");
+    }
+
+    @Test
+    public void dumpXML() {
+        TreeStructureVisitor tsv = new TreeStructureVisitor(
+                FormatInstructionsFactory.getFormatInstructions(Format.XML));
+        cu.accept(tsv, 0);
+        System.out.println(tsv.getResult() + "\n----------------------");
+    }
+
+    @Test
+    public void dumpJSON() {
+        TreeStructureVisitor tsv = new TreeStructureVisitor(
+                FormatInstructionsFactory.getFormatInstructions(Format.JSON));
+        cu.accept(tsv, 0);
+        System.out.println(tsv.getResult() + "\n----------------------");
+    }
+
 }
