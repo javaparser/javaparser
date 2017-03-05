d3c9e35c53b6b0d67aed456ef1c5c23db9b1d04c
Danny van Bruggen <hexagonaal@gmail.com>
2017-03-05 14:59:28 +0100

    Implements extends & implements restrictions

--- javaparser-core/src/main/java/com/github/javaparser/ast/validator/DefaultValidator.java
+++ javaparser-core/src/main/java/com/github/javaparser/ast/validator/DefaultValidator.java
@@ -1,5 +1,6 @@
 package com.github.javaparser.ast.validator;
 
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.stmt.TryStmt;
 
 /**
@@ -7,15 +8,33 @@ import com.github.javaparser.ast.stmt.TryStmt;
  */
 public class DefaultValidator extends Validators {
     public DefaultValidator() {
-        super(new VisitorValidator() {
-            @Override
-            public void visit(TryStmt n, ProblemReporter reporter) {
-                if (n.getCatchClauses().isEmpty()
-                        && n.getResources().isEmpty()
-                        && !n.getFinallyBlock().isPresent()) {
-                    reporter.report("Try has no finally, no catch, and no resources", n);
+        super(
+                new VisitorValidator() {
+                    @Override
+                    public void visit(TryStmt n, ProblemReporter reporter) {
+                        if (n.getCatchClauses().isEmpty()
+                                && n.getResources().isEmpty()
+                                && !n.getFinallyBlock().isPresent()) {
+                            reporter.report("Try has no finally, no catch, and no resources", n);
+                        }
+                    }
+                },
+                new VisitorValidator() {
+                    @Override
+                    public void visit(ClassOrInterfaceDeclaration n, ProblemReporter reporter) {
+                        if (!n.isInterface() && n.getExtendedTypes().size() > 1) {
+                            reporter.report("A class cannot extend more than one other class", n.getExtendedTypes(1));
+                        }
+                    }
+                },
+                new VisitorValidator() {
+                    @Override
+                    public void visit(ClassOrInterfaceDeclaration n, ProblemReporter reporter) {
+                        if (n.isInterface() && !n.getImplementedTypes().isEmpty()) {
+                            reporter.report("An interface cannot implement other interfaces", n.getImplementedTypes(0));
+                        }
+                    }
                 }
-            }
-        });
+        );
     }
 }
