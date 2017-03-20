3491f8fc71482b5e5398d2c2e3b094d5c3d5c200
Danny van Bruggen <hexagonaal@gmail.com>
2017-03-20 23:17:31 +0100

    Inline inner classes validator

--- javaparser-core/src/main/java/com/github/javaparser/ast/validator/Java1_1Validator.java
+++ javaparser-core/src/main/java/com/github/javaparser/ast/validator/Java1_1Validator.java
@@ -1,12 +1,18 @@
 package com.github.javaparser.ast.validator;
 
-import com.github.javaparser.ast.validator.chunks.InnerClassesValidator;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.stmt.LocalClassDeclarationStmt;
 
 /**
  * This validator validates according to Java 1.1 syntax rules.
  */
 public class Java1_1Validator extends Java1_0Validator {
-    protected final Validator innerClasses = new InnerClassesValidator();
+    protected final Validator innerClasses = new SingleNodeTypeValidator<>(ClassOrInterfaceDeclaration.class,
+            (n, reporter) -> n.getParentNode().ifPresent(p -> {
+                if (p instanceof LocalClassDeclarationStmt && n.isInterface())
+                    reporter.report(n, "There is no such thing as a local interface.");
+            })
+    );
 
     public Java1_1Validator() {
         super();
