a4c669d1a68b1a390d8f3f773801d23641098cc7
Bohdan Sharipov <bohdansharipov@gmail.com>
2017-06-21 17:18:19 -0400

    - Undid changes done with shuffling imports to reduce the number of changes done in javaparser project.

--- javaparser-core/src/main/java/com/github/javaparser/ast/visitor/CloneVisitor.java
+++ javaparser-core/src/main/java/com/github/javaparser/ast/visitor/CloneVisitor.java
@@ -22,16 +22,13 @@ package com.github.javaparser.ast.visitor;
 
 import com.github.javaparser.ast.*;
 import com.github.javaparser.ast.body.*;
-import com.github.javaparser.ast.comments.BlockComment;
-import com.github.javaparser.ast.comments.Comment;
-import com.github.javaparser.ast.comments.JavadocComment;
-import com.github.javaparser.ast.comments.LineComment;
+import com.github.javaparser.ast.comments.*;
 import com.github.javaparser.ast.expr.*;
 import com.github.javaparser.ast.modules.*;
 import com.github.javaparser.ast.stmt.*;
 import com.github.javaparser.ast.type.*;
-import javax.annotation.Generated;
 import java.util.Optional;
+import javax.annotation.Generated;
 
 /**
  * A visitor that clones (copies) a node and all its children.
