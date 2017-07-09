4f34bf942bf47b72815f0306463b61bb5fc8ff55
Danny van Bruggen <hexagonaal@gmail.com>
2017-07-09 23:03:32 +0200

    * add workaround for JPMS edge case * correct token type to identifier for funky JPMS keywords in identifier positions

--- javaparser-core/src/main/java/com/github/javaparser/JavaToken.java
+++ javaparser-core/src/main/java/com/github/javaparser/JavaToken.java
@@ -35,7 +35,7 @@ public class JavaToken {
     public static final JavaToken INVALID = new JavaToken();
 
     private final Range range;
-    private final int kind;
+    private int kind;
     private final String text;
     private final Optional<JavaToken> previousToken;
     private Optional<JavaToken> nextToken = Optional.empty();
@@ -108,6 +108,10 @@ public class JavaToken {
 
     public int getKind() {
         return kind;
+    }
+
+    public void setKind(int kind) {
+        this.kind = kind;
     }
 
     public String getText() {
