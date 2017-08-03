96e33a38aef7ef94178f77f3a69a47389a69fc4e
malte_langkabel <malte.langkabel@gmail.com>
2017-08-03 11:25:52 +0200

    remove unused include in JavaParserClassDeclarationTest

--- java-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/javaparsermodel/declarations/JavaParserClassDeclarationTest.java
+++ java-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/javaparsermodel/declarations/JavaParserClassDeclarationTest.java
@@ -42,7 +42,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
