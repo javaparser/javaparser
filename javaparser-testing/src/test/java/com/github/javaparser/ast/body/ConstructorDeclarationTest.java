4b968aedeecdbc5031f4fbbfc7dfcb0f6fcad04c
Danny van Bruggen <hexagonaal@gmail.com>
2017-09-26 21:42:12 +0200

    Give ConstructorDeclaration a nice name-only constructor

--- javaparser-testing/src/test/java/com/github/javaparser/ast/body/ConstructorDeclarationTest.java
+++ javaparser-testing/src/test/java/com/github/javaparser/ast/body/ConstructorDeclarationTest.java
@@ -7,11 +7,10 @@ import static org.junit.Assert.assertEquals;
 public class ConstructorDeclarationTest {
     @Test
     public void acceptsSuper() {
-        ConstructorDeclaration cons = new ConstructorDeclaration()
-                .setName("Cons");
-        cons.createBody()
-                .addStatement("super();");
-        assertEquals("Cons() {\n" +
+        ConstructorDeclaration cons = new ConstructorDeclaration("Cons");
+        cons.createBody().addStatement("super();");
+
+        assertEquals("public Cons() {\n" +
                 "    super();\n" +
                 "}", cons.toString());
     }
