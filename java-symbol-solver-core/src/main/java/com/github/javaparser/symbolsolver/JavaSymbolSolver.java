e6ad15bf8a403b581129a06c30ed92a062991fef
Federico Tomassetti <federico@tomassetti.me>
2017-10-07 14:02:00 +0200

    documenting JavaSymbolSolver

--- java-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/JavaSymbolSolver.java
+++ java-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/JavaSymbolSolver.java
@@ -3,17 +3,21 @@ package com.github.javaparser.symbolsolver;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.type.ArrayType;
 import com.github.javaparser.resolution.SymbolResolver;
-import com.github.javaparser.resolution.types.ResolvedArrayType;
-import com.github.javaparser.resolution.types.ResolvedType;
 import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
-import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFactory;
 import com.github.javaparser.symbolsolver.javaparsermodel.declarations.JavaParserMethodDeclaration;
 import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
 
-import com.github.javaparser.ast.type.ArrayType;
-
-
+/**
+ * This implementation of the SymbolResolver wraps the functionalities of the library to make them easily usable
+ * from JavaParser nodes.
+ *
+ * An instance of this class should be created once and then injected in all the CompilationUnit for which we
+ * want to enable symbol resolution. To do so the method inject can be used.
+ *
+ * @author Federico Tomassetti
+ */
 public class JavaSymbolSolver implements SymbolResolver {
 
     private TypeSolver typeSolver;
@@ -33,6 +37,10 @@ public class JavaSymbolSolver implements SymbolResolver {
         throw new UnsupportedOperationException("Unable to resolve to " + resultClass.getSimpleName() + " from " + node.getClass().getSimpleName());
     }
 
+    /**
+     * Register this SymbolResolver into a CompilationUnit, so that symbol resolution becomes available to
+     * all nodes part of the CompilationUnit.
+     */
     public void inject(CompilationUnit destination) {
         destination.setData(Node.SYMBOL_RESOLVER_KEY, this);
     }
