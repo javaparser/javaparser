228526d84c52f25abd222163f18c34af6fb9d7ed
Federico Tomassetti <federico@tomassetti.me>
2017-10-23 14:08:29 +0200

    resolve compilation issues for supporting JP 3.5.0

--- java-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/JavaSymbolSolver.java
+++ java-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/JavaSymbolSolver.java
@@ -3,8 +3,11 @@ package com.github.javaparser.symbolsolver;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.type.ArrayType;
+import com.github.javaparser.ast.type.Type;
 import com.github.javaparser.resolution.SymbolResolver;
+import com.github.javaparser.resolution.types.ResolvedType;
 import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
 import com.github.javaparser.symbolsolver.javaparsermodel.declarations.JavaParserMethodDeclaration;
 import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
@@ -26,22 +29,34 @@ public class JavaSymbolSolver implements SymbolResolver {
         this.typeSolver = typeSolver;
     }
 
-    @Override
-    public <T> T resolve(Node node, Class<T> resultClass) {
-        if (node instanceof MethodDeclaration) {
-            return resultClass.cast(new JavaParserMethodDeclaration((MethodDeclaration)node, typeSolver));
-        }
-        if (node instanceof ArrayType) {
-            return resultClass.cast(JavaParserFacade.get(typeSolver).convert((ArrayType)node, node));
-        }
-        throw new UnsupportedOperationException("Unable to resolve to " + resultClass.getSimpleName() + " from " + node.getClass().getSimpleName());
-    }
-
     /**
      * Register this SymbolResolver into a CompilationUnit, so that symbol resolution becomes available to
      * all nodes part of the CompilationUnit.
      */
     public void inject(CompilationUnit destination) {
         destination.setData(Node.SYMBOL_RESOLVER_KEY, this);
+    }
+
+    @Override
+    public <T> T resolveDeclaration(Node node, Class<T> resultClass) {
+        if (node instanceof MethodDeclaration) {
+            return resultClass.cast(new JavaParserMethodDeclaration((MethodDeclaration)node, typeSolver));
+        }
+        throw new UnsupportedOperationException("Unable to find the declaration of type " + resultClass.getSimpleName()
+                + " from " + node.getClass().getSimpleName());
+    }
+
+    @Override
+    public <T> T toResolvedType(Type javaparserType, Class<T> resultClass) {
+        if (javaparserType instanceof ArrayType) {
+            return resultClass.cast(JavaParserFacade.get(typeSolver).convert((ArrayType)javaparserType, javaparserType));
+        }
+        throw new UnsupportedOperationException("Unable to get the resolved type of class "
+                + resultClass.getSimpleName() + " from " + javaparserType);
+    }
+
+    @Override
+    public ResolvedType calculateType(Expression expression) {
+        throw new UnsupportedOperationException("Unable to calculate type of " + expression);
     }
 }
