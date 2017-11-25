020cace151b755e599f8216ffce551ce7ab98ffb
Federico Tomassetti <federico@tomassetti.me>
2017-11-25 14:10:17 +0100

    making ResolvedUnionType constructor take a list

--- javaparser-core/src/main/java/com/github/javaparser/resolution/types/ResolvedUnionType.java
+++ javaparser-core/src/main/java/com/github/javaparser/resolution/types/ResolvedUnionType.java
@@ -35,7 +35,7 @@ import java.util.stream.Collectors;
 public class ResolvedUnionType implements ResolvedType {
     private List<ResolvedType> elements;
 
-    public ResolvedUnionType(Collection<ResolvedType> elements) {
+    public ResolvedUnionType(List<ResolvedType> elements) {
         if (elements.size() < 2) {
             throw new IllegalArgumentException("An union type should have at least two elements. This has " + elements.size());
         }
