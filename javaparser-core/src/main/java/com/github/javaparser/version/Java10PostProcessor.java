7377e6e3ac535ea7fbf6f37a536d3e751a7544f9
Danny van Bruggen <hexagonaal@gmail.com>
2018-02-06 21:51:47 +0100

    Fix setup of postprocessors

--- javaparser-core/src/main/java/com/github/javaparser/version/Java10PostProcessor.java
+++ javaparser-core/src/main/java/com/github/javaparser/version/Java10PostProcessor.java
@@ -7,8 +7,6 @@ import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.javaparser.ast.type.VarType;
 import com.github.javaparser.ast.validator.Java10Validator;
 import com.github.javaparser.ast.validator.ProblemReporter;
-import com.github.javaparser.ast.visitor.ModifierVisitor;
-import com.github.javaparser.ast.visitor.Visitable;
 
 import static com.github.javaparser.ParseResult.*;
 
@@ -16,8 +14,8 @@ import static com.github.javaparser.ParseResult.*;
  * Processes the generic AST into a Java 10 AST and validates it.
  */
 public class Java10PostProcessor extends PostProcessors {
-    private final Java10Validator validator = new Java10Validator();
-    private final PostProcessor varNodeCreator = new PostProcessor() {
+    protected final PostProcessor java10Validator = new Java10Validator().postProcessor();
+    protected final PostProcessor varNodeCreator = new PostProcessor() {
         @Override
         public void process(ParseResult<? extends Node> result, ParserConfiguration configuration) {
             result.getResult().ifPresent(node -> {
@@ -30,10 +28,8 @@ public class Java10PostProcessor extends PostProcessors {
         }
     };
 
-    @Override
-    public void process(ParseResult<? extends Node> result, ParserConfiguration configuration) {
+    public Java10PostProcessor() {
         add(varNodeCreator);
-        result.getResult().ifPresent(node ->
-                validator.accept(node, new ProblemReporter(problem -> result.getProblems().add(problem))));
+        add(java10Validator);
     }
 }
--- javaparser-core/src/main/java/com/github/javaparser/version/Java11PostProcessor.java
+++ javaparser-core/src/main/java/com/github/javaparser/version/Java11PostProcessor.java
@@ -3,9 +3,6 @@ package com.github.javaparser.version;
 import com.github.javaparser.ParseResult;
 import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.ast.Node;
-import com.github.javaparser.ast.type.ClassOrInterfaceType;
-import com.github.javaparser.ast.type.VarType;
-import com.github.javaparser.ast.validator.Java10Validator;
 import com.github.javaparser.ast.validator.Java11Validator;
 import com.github.javaparser.ast.validator.ProblemReporter;
 
@@ -13,11 +10,9 @@ import com.github.javaparser.ast.validator.ProblemReporter;
  * Processes the generic AST into a Java 10 AST and validates it.
  */
 public class Java11PostProcessor extends Java10PostProcessor {
-    private final Java11Validator validator = new Java11Validator();
+    protected final ParseResult.PostProcessor java11Validator = new Java11Validator().postProcessor();
 
-    @Override
-    public void process(ParseResult<? extends Node> result, ParserConfiguration configuration) {
-        result.getResult().ifPresent(node ->
-                validator.accept(node, new ProblemReporter(problem -> result.getProblems().add(problem))));
+    public Java11PostProcessor() {
+        replace(java10Validator, java11Validator);
     }
 }
