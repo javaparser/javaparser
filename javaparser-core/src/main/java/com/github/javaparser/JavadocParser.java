2088991cbd70636115717659b2531edc63588174
Danny van Bruggen <hexagonaal@gmail.com>
2018-05-12 01:46:28 +0200

    Accept any amount of whitespace before a Javadoc block tag

--- javaparser-core/src/main/java/com/github/javaparser/JavadocParser.java
+++ javaparser-core/src/main/java/com/github/javaparser/JavadocParser.java
@@ -35,12 +35,12 @@ import static com.github.javaparser.utils.Utils.*;
 import static com.github.javaparser.utils.Utils.nextWord;
 
 /**
- * The class responsible for parsing the content of JavadocComments and produce JavadocDocuments.
+ * The class responsible for parsing the content of JavadocComments and producing JavadocDocuments.
  */
 class JavadocParser {
 
     private static String BLOCK_TAG_PREFIX = "@";
-    private static Pattern BLOCK_PATTERN = Pattern.compile("^" + BLOCK_TAG_PREFIX, Pattern.MULTILINE);
+    private static Pattern BLOCK_PATTERN = Pattern.compile("^\\s*" + BLOCK_TAG_PREFIX, Pattern.MULTILINE);
 
     public static Javadoc parse(JavadocComment comment) {
         return parse(comment.getContent());
