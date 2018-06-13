5c073b93425be995b514b16216c09c24d603713e
Thomas Leu <thomas.leu@gmail.com>
2018-06-14 00:48:10 +0200

    added and currying method to TextElementMatcher

--- javaparser-core/src/main/java/com/github/javaparser/printer/lexicalpreservation/TextElementMatcher.java
+++ javaparser-core/src/main/java/com/github/javaparser/printer/lexicalpreservation/TextElementMatcher.java
@@ -21,8 +21,15 @@
 
 package com.github.javaparser.printer.lexicalpreservation;
 
+import com.github.javaparser.Range;
+
+import java.util.Optional;
+
 public interface TextElementMatcher {
 
     boolean match(TextElement textElement);
 
+    default TextElementMatcher and(TextElementMatcher textElementMatcher) {
+        return textElement -> this.match(textElement) && textElementMatcher.match(textElement);
+    }
 }
