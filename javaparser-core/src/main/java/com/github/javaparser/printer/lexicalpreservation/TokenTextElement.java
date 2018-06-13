241f85a42d58eef9d7674a858b6b2c9116bbcc6b
Thomas Leu <thomas.leu@gmail.com>
2018-06-14 00:47:31 +0200

    added getRange and matchByRange to TextElement

--- javaparser-core/src/main/java/com/github/javaparser/printer/lexicalpreservation/TokenTextElement.java
+++ javaparser-core/src/main/java/com/github/javaparser/printer/lexicalpreservation/TokenTextElement.java
@@ -22,7 +22,10 @@
 package com.github.javaparser.printer.lexicalpreservation;
 
 import com.github.javaparser.JavaToken;
+import com.github.javaparser.Range;
 import com.github.javaparser.ast.Node;
+
+import java.util.Optional;
 
 class TokenTextElement extends TextElement {
     private final JavaToken token;
@@ -110,5 +113,10 @@ class TokenTextElement extends TextElement {
     @Override
     public boolean isChildOfClass(Class<? extends Node> nodeClass) {
         return false;
+    }
+
+    @Override
+    Optional<Range> getRange() {
+        return token.getRange();
     }
 }
