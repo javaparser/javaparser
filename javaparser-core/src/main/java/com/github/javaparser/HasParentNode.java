1ebccb08e3ce73b37213833429e15ed1d864871c
Danny van Bruggen <hexagonaal@gmail.com>
2018-08-14 21:29:02 +0200

    Remove funny import

--- javaparser-core/src/main/java/com/github/javaparser/HasParentNode.java
+++ javaparser-core/src/main/java/com/github/javaparser/HasParentNode.java
@@ -23,7 +23,6 @@ package com.github.javaparser;
 
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.observer.Observable;
-import javafx.scene.Parent;
 
 import java.util.Optional;
 
