4c14be70ecbeae35e4cbe1430d262e196ff5d196
Maarten Coene <maartenc@apache.org>
2019-03-07 16:58:25 +0100

    Fix import statement

--- javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/resolution/MethodsResolutionWithJavassistTest.java
+++ javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/resolution/MethodsResolutionWithJavassistTest.java
@@ -20,7 +20,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MethodsResolutionWithJavassistTest extends AbstractResolutionTest {
 
