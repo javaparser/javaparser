0f5add60ae08a6311d3b0751d7dab5f54e74d3f6
Danny van Bruggen <hexagonaal@gmail.com>
2019-04-10 21:10:34 +0200

    Don't put EOL inside the line comment tokens

--- javaparser-core/src/main/java/com/github/javaparser/printer/lexicalpreservation/Difference.java
+++ javaparser-core/src/main/java/com/github/javaparser/printer/lexicalpreservation/Difference.java
@@ -48,11 +48,10 @@ public class Difference {
     }
 
     private List<TextElement> processIndentation(List<TokenTextElement> indentation, List<TextElement> prevElements) {
-        List<TextElement> res = new LinkedList<>();
-        res.addAll(indentation);
+        List<TextElement> res = new LinkedList<>(indentation);
         boolean afterNl = false;
         for (TextElement e : prevElements) {
-            if (e.isNewline() || e.isToken(SINGLE_LINE_COMMENT)) {
+            if (e.isNewline()) {
                 res.clear();
                 afterNl = true;
             } else {
@@ -589,11 +588,8 @@ public class Difference {
             // Handling trailing comments
             if(nodeText.numberOfElements() > originalIndex + 1 &&
                     nodeText.getTextElement(originalIndex).isComment()) {
-                String expanded = nodeText.getTextElement(originalIndex).expand();
-                if(expanded.startsWith("/*")) {
-                    originalIndex++; // "/*" comments need an extra increment
-                }
-                originalIndex++; // Any trailing comment requires increment of the originalIndex
+                // Need to get behind the comment:
+                originalIndex += 2;
                 nodeText.addElement(originalIndex, addedTextElement); // Defer originalIndex increment
                 // We want to adjust the indentation while considering the new element that we added
                 originalIndex = adjustIndentation(indentation, nodeText, originalIndex, false);
