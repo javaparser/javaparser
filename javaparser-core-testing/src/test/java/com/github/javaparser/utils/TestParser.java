f6d08fc9a3caa178936da1260ce3fc472db02a5d
Danny van Bruggen <hexagonaal@gmail.com>
2019-05-17 19:54:10 +0200

    Implement fully qualified name logic for type declarations

--- javaparser-core-testing/src/test/java/com/github/javaparser/utils/TestParser.java
+++ javaparser-core-testing/src/test/java/com/github/javaparser/utils/TestParser.java
@@ -2,6 +2,7 @@ package com.github.javaparser.utils;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParserConfiguration;
+import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.BodyDeclaration;
 import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.stmt.Statement;
@@ -11,6 +12,10 @@ import static com.github.javaparser.ParserConfiguration.LanguageLevel.BLEEDING_E
 public class TestParser {
 
     private static final JavaParser parser = new JavaParser(new ParserConfiguration().setLanguageLevel(BLEEDING_EDGE));
+
+    public static CompilationUnit parseCompilationUnit(String stmt) {
+        return parser.parse(stmt).getResult().get();
+    }
 
     public static Statement parseStatement(String stmt) {
         return parser.parseStatement(stmt).getResult().get();
