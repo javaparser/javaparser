0a0f5591dce726713cb85f408019517d60775829
Simon <simon.mailadres@gmail.com>
2019-09-18 20:46:37 +0200

    Created test for issue 2366

---
+++ javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/Issue2366Test.java
@@ -0,0 +1,50 @@
+package com.github.javaparser.symbolsolver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Test;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseStart;
+import com.github.javaparser.ParserConfiguration;
+import com.github.javaparser.ParserConfiguration.LanguageLevel;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.StreamProvider;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.expr.ObjectCreationExpr;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.resolution.declarations.ResolvedValueDeclaration;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
+
+class Issue2366Test extends AbstractSymbolResolutionTest {
+
+    @Test
+    void issue2366() throws IOException {
+        Path dir = adaptPath("src/test/resources/issue2366");
+        Path file = adaptPath("src/test/resources/issue2366/Test.java");
+
+        CombinedTypeSolver combinedSolver = new CombinedTypeSolver(new ReflectionTypeSolver());	    
+
+        ParserConfiguration pc = new ParserConfiguration()
+            	                        .setSymbolResolver(new JavaSymbolSolver(combinedSolver))
+            	                        .setLanguageLevel(LanguageLevel.JAVA_8);
+
+        JavaParser javaParser = new JavaParser(pc);
+
+        CompilationUnit unit = javaParser.parse(ParseStart.COMPILATION_UNIT,
+                new StreamProvider(Files.newInputStream(file))).getResult().get();
+
+        unit.accept(new VoidVisitorAdapter<Object>() {
+            @Override
+            public void visit(ObjectCreationExpr exp, Object arg) {
+                System.out.println(exp.resolve().getSignature());
+            }            
+        }, null);
+    }
+}
