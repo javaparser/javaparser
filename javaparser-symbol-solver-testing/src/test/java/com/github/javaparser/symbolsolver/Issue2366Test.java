f0aead80aed1ca4aff3028bcb1fb6e4d6e9f0467
Simon <simon.mailadres@gmail.com>
2019-09-18 21:22:25 +0200

    We actually expect this to throw

--- javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/Issue2366Test.java
+++ javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/Issue2366Test.java
@@ -1,30 +1,28 @@
 package com.github.javaparser.symbolsolver;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseStart;
 import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.ParserConfiguration.LanguageLevel;
-import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.StreamProvider;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.expr.NameExpr;
 import com.github.javaparser.ast.expr.ObjectCreationExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-import com.github.javaparser.resolution.declarations.ResolvedValueDeclaration;
+import com.github.javaparser.resolution.UnsolvedSymbolException;
+import com.github.javaparser.symbolsolver.resolution.SymbolSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 
 class Issue2366Test extends AbstractSymbolResolutionTest {
-
-    @Test
+	
+    @Test()
     void issue2366() throws IOException {
         Path dir = adaptPath("src/test/resources/issue2366");
         Path file = adaptPath("src/test/resources/issue2366/Test.java");
@@ -39,12 +37,17 @@ class Issue2366Test extends AbstractSymbolResolutionTest {
 
         CompilationUnit unit = javaParser.parse(ParseStart.COMPILATION_UNIT,
                 new StreamProvider(Files.newInputStream(file))).getResult().get();
+        
+        ObjectCreationExpr oce = unit.findFirst(ObjectCreationExpr.class).get();
+        
+        SymbolSolver solver = new SymbolSolver(combinedSolver);
+        
 
-        unit.accept(new VoidVisitorAdapter<Object>() {
+        Assertions.assertThrows(UnsolvedSymbolException.class, () -> unit.accept(new VoidVisitorAdapter<Object>() {
             @Override
             public void visit(ObjectCreationExpr exp, Object arg) {
                 System.out.println(exp.resolve().getSignature());
             }            
-        }, null);
+        }, null));
     }
 }
