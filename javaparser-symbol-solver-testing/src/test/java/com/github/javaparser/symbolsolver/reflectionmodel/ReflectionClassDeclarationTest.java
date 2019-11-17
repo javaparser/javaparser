d4ce840d3822966823255a6e6805b28b039cb8db
Roger Howell <howelrtc@aston.ac.uk>
2019-11-17 19:22:37 +0000

    update of the copyright notices to be consistent throughout the javaparser symbol solver modules
    this commit changes the previous commit to credit Federico for 2015-16, then 2017-present is JavaParser per https://github.com/javaparser/javasymbolsolver/issues/316
    
    #1801
    
    https://github.com/javaparser/javasymbolsolver/issues/316
    https://github.com/javaparser/javaparser/pull/1833

--- javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/reflectionmodel/ReflectionClassDeclarationTest.java
+++ javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/reflectionmodel/ReflectionClassDeclarationTest.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2007-2010 Júlio Vilmar Gesser.
- * Copyright (C) 2011, 2013-2019 The JavaParser Team.
+ * Copyright (C) 2015-2016 Federico Tomassetti
+ * Copyright (C) 2017-2019 The JavaParser Team.
  *
  * This file is part of JavaParser.
  *
