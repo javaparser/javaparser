002b96bcf07ee4340177ea828bd6514c81731be3
Roger Howell <howelrtc@aston.ac.uk>
2020-01-21 12:19:43 +0000

    Added test case per issue #1511 -- problem reproduced in v3.15.9

---
+++ javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/Issue1511Test.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2015-2016 Federico Tomassetti
+ * Copyright (C) 2017-2020 The JavaParser Team.
+ *
+ * This file is part of JavaParser.
+ *
+ * JavaParser can be used either under the terms of
+ * a) the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * b) the terms of the Apache License
+ *
+ * You should have received a copy of both licenses in LICENCE.LGPL and
+ * LICENCE.APACHE. Please refer to those files for details.
+ *
+ * JavaParser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ */
+
+package com.github.javaparser.symbolsolver;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.stmt.ExplicitConstructorInvocationStmt;
+import com.github.javaparser.resolution.declarations.ResolvedConstructorDeclaration;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+
+import static com.github.javaparser.symbolsolver.AbstractSymbolResolutionTest.adaptPath;
+
+public class Issue1511Test {
+
+    @Test
+    public void tempTest() throws FileNotFoundException {
+
+        Path dir = adaptPath("src/test/resources/issue1511");
+        Path file = adaptPath("src/test/resources/issue1511/A.java");
+
+        // configure symbol solver
+        CombinedTypeSolver typeSolver = new CombinedTypeSolver();
+        typeSolver.add(new ReflectionTypeSolver());
+        typeSolver.add(new JavaParserTypeSolver(dir.toFile()));
+        JavaSymbolSolver symbolSolver = new JavaSymbolSolver(typeSolver);
+        StaticJavaParser.getConfiguration().setSymbolResolver(symbolSolver);
+
+        // get compilation unit & extract explicit constructor invocation statement
+        CompilationUnit cu = StaticJavaParser.parse(file.toFile());
+        ExplicitConstructorInvocationStmt ecis = cu.getPrimaryType().get()
+            .asClassOrInterfaceDeclaration().getMember(0)
+            .asConstructorDeclaration().getBody().getStatement(0)
+            .asExplicitConstructorInvocationStmt();
+
+        // attempt to resolve explicit constructor invocation statement
+        ResolvedConstructorDeclaration rcd = ecis.resolve(); //.resolveInvokedConstructor(); // <-- exception occurs
+    }
+
+}
