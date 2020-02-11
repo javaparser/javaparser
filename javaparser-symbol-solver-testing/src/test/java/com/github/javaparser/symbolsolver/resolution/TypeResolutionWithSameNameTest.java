475dccbe464104ef20b1f82bbe031c4853065afb
iTakeshi <itakeshi.net@gmail.com>
2020-02-11 09:08:56 +0900

    rename test resource dir

--- javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/resolution/TypeResolutionWithSameNameTest.java
+++ javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/resolution/TypeResolutionWithSameNameTest.java
@@ -40,7 +40,6 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 public class TypeResolutionWithSameNameTest extends AbstractResolutionTest {
 
-
     /*
      * abstract class extends_duplicate.DuplicateTypeName
      * class extends_duplicate.A extends extends_duplicate.DuplicateTypeName
@@ -48,8 +47,8 @@ public class TypeResolutionWithSameNameTest extends AbstractResolutionTest {
      */
     @Test
     void testTypesWithSameNameInPackageAndNested_directExtends() throws IOException {
-        Path srcRootPath = adaptPath("src/test/resources/TypeResolutionWithSameNameTest/src1");
-        Path extendsTypePath = adaptPath("src/test/resources/TypeResolutionWithSameNameTest/src1/extends_duplicate/A.java");
+        Path srcRootPath = adaptPath("src/test/resources/TypeResolutionWithSameNameTest/00_package_precedes_nested_class");
+        Path extendsTypePath = adaptPath("src/test/resources/TypeResolutionWithSameNameTest/00_package_precedes_nested_class/extends_duplicate/A.java");
 
         JavaParserTypeSolver javaParserTypeSolver = new JavaParserTypeSolver(srcRootPath);
         StaticJavaParser
@@ -67,10 +66,9 @@ public class TypeResolutionWithSameNameTest extends AbstractResolutionTest {
         // Verify qualified name matches the non-nested class in the same package.
         // Note verbose assertions show both the "correct" expected value, and the erroneous value to be avoided.
         String qualifiedName = resolvedExtendedType.getQualifiedName();
-        assertEquals("extends_duplicate.DuplicateTypeName", qualifiedName, "Error - not resolved to interface in separate file.");
-        assertNotEquals("extends_duplicate.A.DuplicateTypeName", qualifiedName, "Error - resolved to nested class.");
+        assertEquals("extends_duplicate.DuplicateTypeName", qualifiedName, "Error - not resolved to the class in the package.");
+        assertNotEquals("extends_duplicate.A.DuplicateTypeName", qualifiedName, "Error - mistakenly resolved to a nested class instead of the expected class.");
     }
-
 
     /*
      * interface implements_duplicate.DuplicateTypeName
@@ -79,8 +77,8 @@ public class TypeResolutionWithSameNameTest extends AbstractResolutionTest {
      */
     @Test
     void testTypesWithSameNameInPackageAndNested_directImplements() throws IOException {
-        Path srcRootPath = adaptPath("src/test/resources/TypeResolutionWithSameNameTest/src2");
-        Path implementingTypePath = adaptPath("src/test/resources/TypeResolutionWithSameNameTest/src2/implements_duplicate/A.java");
+        Path srcRootPath = adaptPath("src/test/resources/TypeResolutionWithSameNameTest/01_package_precedes_nested_interface");
+        Path implementingTypePath = adaptPath("src/test/resources/TypeResolutionWithSameNameTest/01_package_precedes_nested_interface/implements_duplicate/A.java");
 
         JavaParserTypeSolver javaParserTypeSolver = new JavaParserTypeSolver(srcRootPath);
         StaticJavaParser
@@ -98,8 +96,8 @@ public class TypeResolutionWithSameNameTest extends AbstractResolutionTest {
         // Verify qualified name matches the non-nested class in the same package.
         // Note verbose assertions show both the "correct" expected value, and the erroneous value to be avoided.
         String qualifiedName = resolvedImplementedType.getQualifiedName();
-        assertEquals("implements_duplicate.DuplicateTypeName", qualifiedName, "Error - not resolved to interface in separate file.");
-        assertNotEquals("implements_duplicate.A.DuplicateTypeName", qualifiedName, "Error - resolved to nested class.");
+        assertEquals("implements_duplicate.DuplicateTypeName", qualifiedName, "Error - not resolved to interface in the package.");
+        assertNotEquals("implements_duplicate.A.DuplicateTypeName", qualifiedName, "Error - mistakenly resolved to a nested class instead of the expected interface.");
     }
 
 
