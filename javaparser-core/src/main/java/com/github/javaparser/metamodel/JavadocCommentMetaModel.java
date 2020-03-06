eed073b87617db96a0cc68de88cbcd5823d2ed38
Danny van Bruggen <55701+matozoid@users.noreply.github.com>
2020-03-06 19:21:08 +0100

    Revert "Merge pull request #2540 from simonsirak/issue_1295_refactor_javadoc_parser_api"
    This reverts commit 9d04816a08b6b7baa4ceb58def64ea9fad6201e9, reversing
    changes made to c849177d5f04a8ac2d77c0b0660f909f711c1106.

--- javaparser-core/src/main/java/com/github/javaparser/metamodel/JavadocCommentMetaModel.java
+++ javaparser-core/src/main/java/com/github/javaparser/metamodel/JavadocCommentMetaModel.java
@@ -27,6 +27,4 @@ public class JavadocCommentMetaModel extends CommentMetaModel {
     JavadocCommentMetaModel(Optional<BaseNodeMetaModel> superBaseNodeMetaModel) {
         super(superBaseNodeMetaModel, com.github.javaparser.ast.comments.JavadocComment.class, "JavadocComment", "com.github.javaparser.ast.comments", false, false);
     }
-
-    public PropertyMetaModel contentNodePropertyMetaModel;
 }
