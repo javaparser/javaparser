11fdb4d5aa648b17880da7b564bb4b3bf5aa75da
MysterAitch <MysterAitch@users.noreply.github.com>
2020-03-27 20:15:04 +0000

    updates to scripts to include ssh/m2 as volumes

--- trigger_build.sh
+++ trigger_build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+echo ${pwd}
+
 docker build --tag jpbuild:1.1 --file ./dev-files/docker_trigger_release/Dockerfile . 
 
-docker run jpbuild:1.1
+docker run \
+    -v /home/jp-docker/.m2:/root/.m2 \
+    -v /home/jp-docker/.ssh:/root/.ssh \
+    jpbuild:1.1
