e3b1de2df93f0b0c9b879147867b017499d7fbe4
Roger Howell <howelrtc@aston.ac.uk>
2020-04-21 21:51:55 +0100

    edited CollectionStrategy.java to remove the static import, making it obvious that the parse method being called is actually StaticJavaParser.parse and not some utility method or similar.

--- javaparser-core/src/main/java/com/github/javaparser/utils/CollectionStrategy.java
+++ javaparser-core/src/main/java/com/github/javaparser/utils/CollectionStrategy.java
@@ -22,6 +22,7 @@
 package com.github.javaparser.utils;
 
 import com.github.javaparser.ParseProblemException;
+import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 
 import java.io.FileNotFoundException;
@@ -29,8 +30,6 @@ import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
 import java.util.Optional;
-
-import static com.github.javaparser.StaticJavaParser.parse;
 
 /**
  * A strategy for discovering the structure of a project.
@@ -42,7 +41,8 @@ public interface CollectionStrategy {
 
     default Optional<Path> getRoot(Path file) throws FileNotFoundException {
         try {
-            return parse(file.toFile()).getStorage()
+            return StaticJavaParser.parse(file.toFile())
+                    .getStorage()
                     .map(CompilationUnit.Storage::getSourceRoot);
         } catch (ParseProblemException e) {
             Log.info("Problem parsing file %s", () -> file);
