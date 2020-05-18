463d704881ab5059ba2249a918a7bfd0c4bdbec4
Roger Howell <howelrtc@aston.ac.uk>
2020-05-18 21:41:06 +0100

    Added test case, comments, and fixed a few bugs...

---
+++ javaparser-core-testing/src/test/java/com/github/javaparser/utils/LineEndingTest.java
@@ -0,0 +1,72 @@
+package com.github.javaparser.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+/**
+ * @author Roger Howell
+ */
+class LineEndingTest {
+
+    @Test
+    void escaped() {
+        assertEquals("\\r", LineEnding.CR.escaped());
+        assertEquals("\\n", LineEnding.LF.escaped());
+        assertEquals("\\r\\n", LineEnding.CRLF.escaped());
+    }
+
+    @Test
+    void lookup() {
+        assertEquals(LineEnding.CR, LineEnding.lookup("\r").get());
+        assertEquals(LineEnding.LF, LineEnding.lookup("\n").get());
+        assertEquals(LineEnding.CRLF, LineEnding.lookup("\r\n").get());
+
+        assertFalse(LineEnding.lookup("").isPresent());
+        assertFalse(LineEnding.lookup("\\r").isPresent());
+        assertFalse(LineEnding.lookup("\\n").isPresent());
+        assertFalse(LineEnding.lookup("test 123 123").isPresent());
+        assertFalse(LineEnding.lookup("\r \n").isPresent());
+    }
+
+    @Test
+    void detect() {
+        assertEquals(LineEnding.CR, LineEnding.detect("\r"));
+        assertEquals(LineEnding.LF, LineEnding.detect("\n"));
+        assertEquals(LineEnding.CRLF, LineEnding.detect("\r\n"));
+
+        assertEquals(LineEnding.NONE, LineEnding.detect(""));
+        assertEquals(LineEnding.NONE, LineEnding.detect("test 123 123"));
+
+        assertEquals(LineEnding.MIXED, LineEnding.detect("\r \n"));
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("\r", LineEnding.CR.toString());
+        assertEquals("\n", LineEnding.LF.toString());
+        assertEquals("\r\n", LineEnding.CRLF.toString());
+
+        // Note that this represents an "arbitrary" line ending -- this test is to highlight any time that it changes.
+        assertEquals("\n", LineEnding.ARBITRARY.toString());
+    }
+
+    @Test
+    void values() {
+        assertEquals(8, LineEnding.values().length);
+    }
+
+    @Test
+    void valueOf() {
+        assertEquals(LineEnding.CR, LineEnding.valueOf("CR"));
+        assertEquals(LineEnding.LF, LineEnding.valueOf("LF"));
+        assertEquals(LineEnding.CRLF, LineEnding.valueOf("CRLF"));
+        assertEquals(LineEnding.NONE, LineEnding.valueOf("NONE"));
+        assertEquals(LineEnding.MIXED, LineEnding.valueOf("MIXED"));
+        assertEquals(LineEnding.SYSTEM, LineEnding.valueOf("SYSTEM"));
+        assertEquals(LineEnding.UNKNOWN, LineEnding.valueOf("UNKNOWN"));
+        assertEquals(LineEnding.ARBITRARY, LineEnding.valueOf("ARBITRARY"));
+    }
+
+}
--- javaparser-core/src/main/java/com/github/javaparser/utils/LineEnding.java
+++ javaparser-core/src/main/java/com/github/javaparser/utils/LineEnding.java
@@ -2,13 +2,49 @@ package com.github.javaparser.utils;
 
 import java.util.Optional;
 
+/**
+ * A representation of line endings, that can be used throughout the codebase.
+ * <br>This is to replace {@code Utils.EOL} which is not explicit in representing the system's EOL character.
+ * <br>It also exposes helper methods for, e.g., detection of the line ending of a given string.
+ *
+ * @author Roger Howell
+ * @see <a href="https://github.com/javaparser/javaparser/issues/2647">https://github.com/javaparser/javaparser/issues/2647</a>
+ */
 public enum LineEnding {
+    /**
+     * The CR {@code \r} line ending is the default line separator for classic MacOS
+     */
     CR("\r"),
+    /**
+     * The LF {@code \n} line ending is the default line separator for Unix and modern MacOS
+     */
     LF("\n"),
+    /**
+     * The CRLF {@code \r\n} line ending is the default line separator for Windows
+     */
     CRLF("\r\n"),
+    /**
+     * This line ending is set to whatever the host system's line separator is
+     */
     SYSTEM(System.getProperty("line.separator")),
+    /**
+     * The ARBITRARY line ending can be used where we do not care about the line separator, only that we use the same
+     * one consistently
+     */
+    ARBITRARY("\n"),
+    /**
+     * The MIXED line ending is used where strings appear to have multiple different line separators e.g. {@code "line
+     * 1\nline 2\rline 3\r\n"} or {@code "line 1\nline 2\rline 3\nline 4\n"}
+     */
     MIXED(""),
+    /**
+     * The UNKNOWN line ending can be used in the case where the given string has not yet been analysed to determine its
+     * line separator
+     */
     UNKNOWN(""),
+    /**
+     * The NONE line ending is used where there are precisely zero line endings e.g. a simple one-line string
+     */
     NONE("");
 
     private final String text;
@@ -18,9 +54,18 @@ public enum LineEnding {
     }
 
     public String escaped() {
-        return text.replaceAll("\\\\", "\\\\\\\\");
+        String result = text
+                .replace("\r", "\\r")
+                .replace("\n", "\\n");
+
+        return result;
     }
 
+    /**
+     * @param ending A string containing ONLY the line separator needle (e.g. {@code \r}, {@code \n}, or {@code \r\n})
+     * @return Where the given ending is a "standard" line separator (i.e. {@code \r}, {@code \n}, or {@code \r\n}),
+     * return that. Otherwise an empty optional.
+     */
     public static Optional<LineEnding> lookup(String ending) {
         if (CR.toString().equals(ending)) {
             return Optional.of(CR);
@@ -33,16 +78,18 @@ public enum LineEnding {
         }
     }
 
+    /**
+     * @return The number of times that the given needle is found within the haystack.
+     */
     private static int count(String haystack, String needle) {
+        // Note that if the needle is multiple characters, e.g. \r\n, the difference in string length will be disproportionately affected.
         return (haystack.length() - haystack.replaceAll(needle, "").length()) / needle.length();
     }
 
     public static LineEnding detect(String string) {
-
         int countCr = count(string, "\r");
-        int countLf = count(string, "\r");
+        int countLf = count(string, "\n");
         int countCrLf = count(string, "\r\n");
-
 
         boolean noLineEndings = countCr == 0 && countLf == 0 && countCrLf == 0;
         if (noLineEndings) {
@@ -57,7 +104,9 @@ public enum LineEnding {
         if (lfOnly) {
             return LF;
         }
-        boolean crLfOnly = countCr == 0 && countLf == 0 && countCrLf > 0;
+
+        // Note that wherever \r\n are found, there will also be an equal number of \r and \n characters found.
+        boolean crLfOnly = countCr == countLf && countLf == countCrLf;
         if (crLfOnly) {
             return CRLF;
         }
