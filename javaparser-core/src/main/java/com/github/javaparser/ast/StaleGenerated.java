9c7c6ba60983f55c73bd05dd1cb9c1edcc74d774
Roger Howell <howelrtc@aston.ac.uk>
2020-05-27 17:35:12 +0100

    fixed comments and content of StaleGenerated.java

--- javaparser-core/src/main/java/com/github/javaparser/ast/StaleGenerated.java
+++ javaparser-core/src/main/java/com/github/javaparser/ast/StaleGenerated.java
@@ -30,17 +30,12 @@ import static java.lang.annotation.RetentionPolicy.SOURCE;
 /**
  * Copied from javax.annotation.Generated and reduced a bit.
  * <p>
- * Indicates a part of the code that was generated,
- * and will be overwritten the next time the generators are run.
+ * Indicates a part of code that was previously marked as {@link Generated},
+ * but a later run of the generator did not re-generate this (thus is "stale").
  */
 @Retention(SOURCE)
 @Target({PACKAGE, TYPE, ANNOTATION_TYPE, METHOD, CONSTRUCTOR, FIELD,
         LOCAL_VARIABLE, PARAMETER})
 public @interface StaleGenerated {
-    /**
-     * The value element must have the name of the code generator.
-     * The recommended convention is to use the fully qualified name of the
-     * code generator. For example: {@code com.acme.generator.CodeGen}.
-     */
-    String[] value();
+
 }
