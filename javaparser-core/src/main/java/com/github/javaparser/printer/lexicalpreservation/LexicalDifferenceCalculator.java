242d06201163fec4b9b584c7c0738a9bdab241e2
Roger Howell <howelrtc@aston.ac.uk>
2020-05-30 16:08:15 +0100

    variable rename

--- javaparser-core/src/main/java/com/github/javaparser/printer/lexicalpreservation/LexicalDifferenceCalculator.java
+++ javaparser-core/src/main/java/com/github/javaparser/printer/lexicalpreservation/LexicalDifferenceCalculator.java
@@ -363,8 +363,8 @@ class LexicalDifferenceCalculator {
             DifferenceElement differenceElement = differenceElements.get(i);
             if (differenceElement.isAdded()) {
                 CsmElement element = differenceElement.getElement();
-                boolean isWhitespaceToken = element instanceof CsmToken && ((CsmToken) element).isNewLine();
-                if (isWhitespaceToken) {
+                boolean isNewlineToken = element instanceof CsmToken && ((CsmToken) element).isNewLine();
+                if (isNewlineToken) {
                     differenceElements.set(i, new Added(CsmElement.newline(lineSeparator)));
                 }
             }
