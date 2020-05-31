b053f6f081f3ddec9974ad693140e80074706b04
Roger Howell <howelrtc@aston.ac.uk>
2020-05-31 19:46:47 +0100

    Made MetaModelGenerator.java extend Generator.java (within the other module, thus also edited the pom to reflect this). See also #2705.

--- javaparser-core-metamodel-generator/src/main/java/com/github/javaparser/generator/metamodel/MetaModelGenerator.java
+++ javaparser-core-metamodel-generator/src/main/java/com/github/javaparser/generator/metamodel/MetaModelGenerator.java
@@ -23,16 +23,11 @@ package com.github.javaparser.generator.metamodel;
 
 import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.StaticJavaParser;
-import com.github.javaparser.ast.*;
-import com.github.javaparser.ast.body.*;
-import com.github.javaparser.ast.comments.BlockComment;
-import com.github.javaparser.ast.comments.Comment;
-import com.github.javaparser.ast.comments.JavadocComment;
-import com.github.javaparser.ast.comments.LineComment;
-import com.github.javaparser.ast.expr.*;
-import com.github.javaparser.ast.modules.*;
-import com.github.javaparser.ast.stmt.*;
-import com.github.javaparser.ast.type.*;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.printer.PrettyPrinter;
 import com.github.javaparser.printer.PrettyPrinterConfiguration;
 import com.github.javaparser.utils.SourceRoot;
@@ -46,7 +41,7 @@ import java.util.List;
 
 import static com.github.javaparser.utils.Utils.decapitalize;
 
-public class MetaModelGenerator {
+public class MetaModelGenerator extends com.github.javaparser.generator.Generator {
     static final String BASE_NODE_META_MODEL = "BaseNodeMetaModel";
 
     static final String COPYRIGHT_NOTICE_JP_CORE = "\n" +
@@ -91,136 +86,144 @@ public class MetaModelGenerator {
         " * GNU Lesser General Public License for more details.\n" +
         " ";
 
+
+    /**
+     * Note that order of this list is manually set and maintained.
+     */
     private static List<Class<? extends Node>> ALL_NODE_CLASSES = new ArrayList<Class<? extends Node>>() {{
         /* Base classes go first, so we don't have to do any sorting to make sure
          generated classes can refer to their base generated classes without
          being afraid those are not initialized yet. */
-        add(Node.class);
 
-        add(BodyDeclaration.class);
-        add(CallableDeclaration.class);
-        add(Statement.class);
-        add(Expression.class);
-        add(Type.class);
-
-        add(AnnotationExpr.class);
-        add(TypeDeclaration.class);
-        add(ReferenceType.class);
-
-        add(LiteralExpr.class);
-        add(LiteralStringValueExpr.class);
-        add(StringLiteralExpr.class);
-
-        add(ModuleDeclaration.class);
-        add(ModuleDirective.class);
+        // Fully qualified names used to make logical groupings (somewhat) more explicit.
 
         //
-        add(ArrayCreationLevel.class);
-        add(CompilationUnit.class);
-        add(PackageDeclaration.class);
-        add(Modifier.class);
+        add(com.github.javaparser.ast.Node.class);
 
-        add(AnnotationDeclaration.class);
-        add(AnnotationMemberDeclaration.class);
-        add(ClassOrInterfaceDeclaration.class);
-        add(ConstructorDeclaration.class);
-        add(EnumConstantDeclaration.class);
-        add(EnumDeclaration.class);
-        add(FieldDeclaration.class);
-        add(InitializerDeclaration.class);
-        add(MethodDeclaration.class);
-        add(Parameter.class);
-        add(ReceiverParameter.class);
-        add(VariableDeclarator.class);
+        add(com.github.javaparser.ast.body.BodyDeclaration.class);
+        add(com.github.javaparser.ast.body.CallableDeclaration.class);
+        add(com.github.javaparser.ast.expr.Expression.class);
+        add(com.github.javaparser.ast.stmt.Statement.class);
+        add(com.github.javaparser.ast.type.Type.class);
 
-        add(Comment.class);
-        add(BlockComment.class);
-        add(JavadocComment.class);
-        add(LineComment.class);
+        add(com.github.javaparser.ast.expr.AnnotationExpr.class);
+        add(com.github.javaparser.ast.type.ReferenceType.class);
+        add(com.github.javaparser.ast.body.TypeDeclaration.class);
 
-        add(ArrayAccessExpr.class);
-        add(ArrayCreationExpr.class);
-        add(ArrayInitializerExpr.class);
-        add(AssignExpr.class);
-        add(BinaryExpr.class);
-        add(BooleanLiteralExpr.class);
-        add(CastExpr.class);
-        add(CharLiteralExpr.class);
-        add(ClassExpr.class);
-        add(ConditionalExpr.class);
-        add(DoubleLiteralExpr.class);
-        add(EnclosedExpr.class);
-        add(FieldAccessExpr.class);
-        add(InstanceOfExpr.class);
-        add(IntegerLiteralExpr.class);
-        add(LambdaExpr.class);
-        add(LongLiteralExpr.class);
-        add(MarkerAnnotationExpr.class);
-        add(MemberValuePair.class);
-        add(MethodCallExpr.class);
-        add(MethodReferenceExpr.class);
-        add(NameExpr.class);
-        add(Name.class);
-        add(NormalAnnotationExpr.class);
-        add(NullLiteralExpr.class);
-        add(ObjectCreationExpr.class);
-        add(SimpleName.class);
-        add(SingleMemberAnnotationExpr.class);
-        add(SuperExpr.class);
-        add(TextBlockLiteralExpr.class);
-        add(ThisExpr.class);
-        add(TypeExpr.class);
-        add(UnaryExpr.class);
-        add(VariableDeclarationExpr.class);
-        add(SwitchExpr.class);
+        add(com.github.javaparser.ast.expr.LiteralExpr.class);
+        add(com.github.javaparser.ast.expr.LiteralStringValueExpr.class);
+        add(com.github.javaparser.ast.expr.StringLiteralExpr.class);
 
-        add(ImportDeclaration.class);
+        add(com.github.javaparser.ast.modules.ModuleDeclaration.class);
+        add(com.github.javaparser.ast.modules.ModuleDirective.class);
 
-        add(AssertStmt.class);
-        add(BlockStmt.class);
-        add(BreakStmt.class);
-        add(CatchClause.class);
-        add(ContinueStmt.class);
-        add(DoStmt.class);
-        add(EmptyStmt.class);
-        add(ExplicitConstructorInvocationStmt.class);
-        add(ExpressionStmt.class);
-        add(ForEachStmt.class);
-        add(ForStmt.class);
-        add(IfStmt.class);
-        add(LabeledStmt.class);
-        add(ReturnStmt.class);
-        add(SwitchEntry.class);
-        add(SwitchStmt.class);
-        add(SynchronizedStmt.class);
-        add(ThrowStmt.class);
-        add(TryStmt.class);
-        add(LocalClassDeclarationStmt.class);
-        add(WhileStmt.class);
-        add(YieldStmt.class);
-        add(UnparsableStmt.class);
+        //
+        add(com.github.javaparser.ast.ArrayCreationLevel.class);
+        add(com.github.javaparser.ast.CompilationUnit.class);
+        add(com.github.javaparser.ast.ImportDeclaration.class);
+        add(com.github.javaparser.ast.Modifier.class);
+        add(com.github.javaparser.ast.PackageDeclaration.class);
 
-        add(ArrayType.class);
-        add(ClassOrInterfaceType.class);
-        add(IntersectionType.class);
-        add(PrimitiveType.class);
-        add(TypeParameter.class);
-        add(UnionType.class);
-        add(UnknownType.class);
-        add(VoidType.class);
-        add(WildcardType.class);
-        add(VarType.class);
+        //
+        add(com.github.javaparser.ast.body.AnnotationDeclaration.class);
+        add(com.github.javaparser.ast.body.AnnotationMemberDeclaration.class);
+        add(com.github.javaparser.ast.body.ClassOrInterfaceDeclaration.class);
+        add(com.github.javaparser.ast.body.ConstructorDeclaration.class);
+        add(com.github.javaparser.ast.body.EnumConstantDeclaration.class);
+        add(com.github.javaparser.ast.body.EnumDeclaration.class);
+        add(com.github.javaparser.ast.body.FieldDeclaration.class);
+        add(com.github.javaparser.ast.body.InitializerDeclaration.class);
+        add(com.github.javaparser.ast.body.MethodDeclaration.class);
+        add(com.github.javaparser.ast.body.Parameter.class);
+        add(com.github.javaparser.ast.body.ReceiverParameter.class);
+        add(com.github.javaparser.ast.body.VariableDeclarator.class);
 
-        add(ModuleRequiresDirective.class);
-        add(ModuleExportsDirective.class);
-        add(ModuleProvidesDirective.class);
-        add(ModuleUsesDirective.class);
-        add(ModuleOpensDirective.class);
+        add(com.github.javaparser.ast.comments.Comment.class); // Base of other comment types
+        add(com.github.javaparser.ast.comments.BlockComment.class);
+        add(com.github.javaparser.ast.comments.LineComment.class);
+        add(com.github.javaparser.ast.comments.JavadocComment.class);
+
+        add(com.github.javaparser.ast.expr.ArrayAccessExpr.class);
+        add(com.github.javaparser.ast.expr.ArrayCreationExpr.class);
+        add(com.github.javaparser.ast.expr.ArrayInitializerExpr.class);
+        add(com.github.javaparser.ast.expr.AssignExpr.class);
+        add(com.github.javaparser.ast.expr.BinaryExpr.class);
+        add(com.github.javaparser.ast.expr.BooleanLiteralExpr.class);
+        add(com.github.javaparser.ast.expr.CastExpr.class);
+        add(com.github.javaparser.ast.expr.CharLiteralExpr.class);
+        add(com.github.javaparser.ast.expr.ClassExpr.class);
+        add(com.github.javaparser.ast.expr.ConditionalExpr.class);
+        add(com.github.javaparser.ast.expr.DoubleLiteralExpr.class);
+        add(com.github.javaparser.ast.expr.EnclosedExpr.class);
+        add(com.github.javaparser.ast.expr.FieldAccessExpr.class);
+        add(com.github.javaparser.ast.expr.InstanceOfExpr.class);
+        add(com.github.javaparser.ast.expr.IntegerLiteralExpr.class);
+        add(com.github.javaparser.ast.expr.LambdaExpr.class);
+        add(com.github.javaparser.ast.expr.LongLiteralExpr.class);
+        add(com.github.javaparser.ast.expr.MarkerAnnotationExpr.class);
+        add(com.github.javaparser.ast.expr.MemberValuePair.class);
+        add(com.github.javaparser.ast.expr.MethodCallExpr.class);
+        add(com.github.javaparser.ast.expr.MethodReferenceExpr.class);
+        add(com.github.javaparser.ast.expr.NameExpr.class);
+        add(com.github.javaparser.ast.expr.Name.class);
+        add(com.github.javaparser.ast.expr.NormalAnnotationExpr.class);
+        add(com.github.javaparser.ast.expr.NullLiteralExpr.class);
+        add(com.github.javaparser.ast.expr.ObjectCreationExpr.class);
+        add(com.github.javaparser.ast.expr.SimpleName.class);
+        add(com.github.javaparser.ast.expr.SingleMemberAnnotationExpr.class);
+        add(com.github.javaparser.ast.expr.SuperExpr.class);
+        add(com.github.javaparser.ast.expr.TextBlockLiteralExpr.class);
+        add(com.github.javaparser.ast.expr.ThisExpr.class);
+        add(com.github.javaparser.ast.expr.TypeExpr.class);
+        add(com.github.javaparser.ast.expr.UnaryExpr.class);
+        add(com.github.javaparser.ast.expr.VariableDeclarationExpr.class);
+        add(com.github.javaparser.ast.expr.SwitchExpr.class);
+
+        add(com.github.javaparser.ast.stmt.AssertStmt.class);
+        add(com.github.javaparser.ast.stmt.BlockStmt.class);
+        add(com.github.javaparser.ast.stmt.BreakStmt.class);
+        add(com.github.javaparser.ast.stmt.CatchClause.class);
+        add(com.github.javaparser.ast.stmt.ContinueStmt.class);
+        add(com.github.javaparser.ast.stmt.DoStmt.class);
+        add(com.github.javaparser.ast.stmt.EmptyStmt.class);
+        add(com.github.javaparser.ast.stmt.ExplicitConstructorInvocationStmt.class);
+        add(com.github.javaparser.ast.stmt.ExpressionStmt.class);
+        add(com.github.javaparser.ast.stmt.ForEachStmt.class);
+        add(com.github.javaparser.ast.stmt.ForStmt.class);
+        add(com.github.javaparser.ast.stmt.IfStmt.class);
+        add(com.github.javaparser.ast.stmt.LabeledStmt.class);
+        add(com.github.javaparser.ast.stmt.LocalClassDeclarationStmt.class);
+        add(com.github.javaparser.ast.stmt.ReturnStmt.class);
+        add(com.github.javaparser.ast.stmt.SwitchEntry.class);
+        add(com.github.javaparser.ast.stmt.SwitchStmt.class);
+        add(com.github.javaparser.ast.stmt.SynchronizedStmt.class);
+        add(com.github.javaparser.ast.stmt.ThrowStmt.class);
+        add(com.github.javaparser.ast.stmt.TryStmt.class);
+        add(com.github.javaparser.ast.stmt.UnparsableStmt.class);
+        add(com.github.javaparser.ast.stmt.WhileStmt.class);
+        add(com.github.javaparser.ast.stmt.YieldStmt.class);
+
+        add(com.github.javaparser.ast.type.ArrayType.class);
+        add(com.github.javaparser.ast.type.ClassOrInterfaceType.class);
+        add(com.github.javaparser.ast.type.IntersectionType.class);
+        add(com.github.javaparser.ast.type.PrimitiveType.class);
+        add(com.github.javaparser.ast.type.TypeParameter.class);
+        add(com.github.javaparser.ast.type.UnionType.class);
+        add(com.github.javaparser.ast.type.UnknownType.class);
+        add(com.github.javaparser.ast.type.VarType.class);
+        add(com.github.javaparser.ast.type.VoidType.class);
+        add(com.github.javaparser.ast.type.WildcardType.class);
+
+        add(com.github.javaparser.ast.modules.ModuleExportsDirective.class);
+        add(com.github.javaparser.ast.modules.ModuleOpensDirective.class);
+        add(com.github.javaparser.ast.modules.ModuleProvidesDirective.class);
+        add(com.github.javaparser.ast.modules.ModuleRequiresDirective.class);
+        add(com.github.javaparser.ast.modules.ModuleUsesDirective.class);
     }};
 
     static String METAMODEL_PACKAGE = "com.github.javaparser.metamodel";
 
-    public static void main(String[] args) throws NoSuchMethodException {
+    public static void main(String[] args) throws Exception {
         if (args.length != 1) {
             throw new RuntimeException("Need 1 parameter: the JavaParser source checkout root directory.");
         }
@@ -232,12 +235,16 @@ public class MetaModelGenerator {
         sourceRoot.setPrinter(new PrettyPrinter(new PrettyPrinterConfiguration().setEndOfLineCharacter("\n"))::print);
         StaticJavaParser.setConfiguration(parserConfiguration);
 
-        new MetaModelGenerator().run(sourceRoot);
+        new MetaModelGenerator(sourceRoot).generate();
 
         sourceRoot.saveAll();
     }
 
-    private void run(SourceRoot sourceRoot) throws NoSuchMethodException {
+    public MetaModelGenerator(SourceRoot sourceRoot) {
+        super(sourceRoot);
+    }
+
+    public void generate() throws Exception {
         final CompilationUnit javaParserMetaModelCu = sourceRoot.parse(METAMODEL_PACKAGE, "JavaParserMetaModel.java");
         javaParserMetaModelCu.setBlockComment(COPYRIGHT_NOTICE_JP_CORE);
 
