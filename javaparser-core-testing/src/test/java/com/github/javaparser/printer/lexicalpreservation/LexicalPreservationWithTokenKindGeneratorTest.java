a4480e0f48e8401d37df89238a26f1cfd2298c58
Roger Howell <howelrtc@aston.ac.uk>
2020-06-10 16:55:27 +0100

    split test into two -- one for each group of removals

--- javaparser-core-testing/src/test/java/com/github/javaparser/printer/lexicalpreservation/LexicalPreservationWithTokenKindGeneratorTest.java
+++ javaparser-core-testing/src/test/java/com/github/javaparser/printer/lexicalpreservation/LexicalPreservationWithTokenKindGeneratorTest.java
@@ -18,7 +18,7 @@ public class LexicalPreservationWithTokenKindGeneratorTest {
 
 
     @Test
-    public void foo() {
+    public void foo_enumOnly() {
         final String originalCode = "" +
                 "public class JavaToken {\n" +
                 "\n" +
@@ -78,6 +78,123 @@ public class LexicalPreservationWithTokenKindGeneratorTest {
                 "\n" +
                 "        public static Kind valueOf(int kind) {\n" +
                 "            switch(kind) {\n" +
+                "                case 146:\n" +
+                "                    return CTRL_Z;\n" +
+                "                case 5:\n" +
+                "                    return SINGLE_LINE_COMMENT;\n" +
+                "                case 4:\n" +
+                "                    return OLD_MAC_EOL;\n" +
+                "                case 3:\n" +
+                "                    return UNIX_EOL;\n" +
+                "                case 2:\n" +
+                "                    return WINDOWS_EOL;\n" +
+                "                case 1:\n" +
+                "                    return SPACE;\n" +
+                "                case 0:\n" +
+                "                    return EOF;\n" +
+                "                default:\n" +
+                "                    throw new IllegalArgumentException(f(\"Token kind %i is unknown.\", kind));\n" +
+                "            }\n" +
+                "        }\n" +
+                "    }\n" +
+                "\n" +
+                "}\n" +
+                "";
+
+        final JavaParser javaParser = new JavaParser(new ParserConfiguration()
+                .setLanguageLevel(RAW)
+                .setLexicalPreservationEnabled(true)
+        );
+
+        final ParseResult<CompilationUnit> parseResult = javaParser.parse(originalCode);
+        final CompilationUnit javaTokenCu = parseResult.getResult().orElseThrow(RuntimeException::new);
+        final ClassOrInterfaceDeclaration javaTokenCoid = javaTokenCu.getClassByName("JavaToken").orElseThrow(() -> new AssertionError("Can't find class in java file."));
+
+        final EnumDeclaration kindEnum = javaTokenCoid
+                .findFirst(EnumDeclaration.class, e -> e.getNameAsString().equals("Kind"))
+                .orElseThrow(() -> new AssertionError("Can't find class in java file."));
+
+        final MethodDeclaration valueOfMethodDeclaration = kindEnum.getMethodsByName("valueOf").get(0);
+        final SwitchStmt valueOfSwitch = valueOfMethodDeclaration
+                .findFirst(SwitchStmt.class)
+                .orElseThrow(() -> new AssertionError("Can't find valueOf switch."));
+
+
+        // Reset the enum:
+        kindEnum.getEntries().clear();
+
+//        assertEquals(originalCode, javaTokenCu.toString());
+        assertEqualsStringIgnoringEol(expectedOutput_lexical, LexicalPreservingPrinter.print(javaTokenCu));
+
+    }
+
+    @Test
+    public void foo_switchOnly() {
+        final String originalCode = "" +
+                "public class JavaToken {\n" +
+                "\n" +
+                "    public enum Kind {\n" +
+                "\n" +
+                "        EOF(0),\n" +
+                "        SPACE(1),\n" +
+                "        WINDOWS_EOL(2),\n" +
+                "        UNIX_EOL(3),\n" +
+                "        OLD_MAC_EOL(4),\n" +
+                "        SINGLE_LINE_COMMENT(5),\n" +
+                "        CTRL_Z(146);\n" +
+                "\n" +
+                "        private final int kind;\n" +
+                "\n" +
+                "        Kind(int kind) {\n" +
+                "            this.kind = kind;\n" +
+                "        }\n" +
+                "\n" +
+                "        public static Kind valueOf(int kind) {\n" +
+                "            switch(kind) {\n" +
+                "                case 146:\n" +
+                "                    return CTRL_Z;\n" +
+                "                case 5:\n" +
+                "                    return SINGLE_LINE_COMMENT;\n" +
+                "                case 4:\n" +
+                "                    return OLD_MAC_EOL;\n" +
+                "                case 3:\n" +
+                "                    return UNIX_EOL;\n" +
+                "                case 2:\n" +
+                "                    return WINDOWS_EOL;\n" +
+                "                case 1:\n" +
+                "                    return SPACE;\n" +
+                "                case 0:\n" +
+                "                    return EOF;\n" +
+                "                default:\n" +
+                "                    throw new IllegalArgumentException(f(\"Token kind %i is unknown.\", kind));\n" +
+                "            }\n" +
+                "        }\n" +
+                "    }\n" +
+                "\n" +
+                "}\n" +
+                "";
+
+
+        final String expectedOutput_lexical = "public class JavaToken {\n" +
+                "\n" +
+                "    public enum Kind {\n" +
+                "\n" +
+                "        EOF(0),\n" +
+                "        SPACE(1),\n" +
+                "        WINDOWS_EOL(2),\n" +
+                "        UNIX_EOL(3),\n" +
+                "        OLD_MAC_EOL(4),\n" +
+                "        SINGLE_LINE_COMMENT(5),\n" +
+                "        CTRL_Z(146);\n" +
+                "\n" +
+                "        private final int kind;\n" +
+                "\n" +
+                "        Kind(int kind) {\n" +
+                "            this.kind = kind;\n" +
+                "        }\n" +
+                "\n" +
+                "        public static Kind valueOf(int kind) {\n" +
+                "            switch(kind) {\n" +
                 "                default:\n" +
                 "                    throw new IllegalArgumentException(f(\"Token kind %i is unknown.\", kind));\n" +
 //                "\n" +
@@ -113,12 +230,6 @@ public class LexicalPreservationWithTokenKindGeneratorTest {
                 .findFirst(SwitchStmt.class)
                 .orElseThrow(() -> new AssertionError("Can't find valueOf switch."));
 
-
-        // TODO: Define "reset"
-        // Reset the enum:
-        kindEnum.getEntries().clear();
-        // Reset the switch within the method valueOf(), leaving only the default
-//        valueOfSwitch.getEntries().stream().filter(e -> e.getLabels().isNonEmpty()).forEach(Node::remove);
 
         // TODO: Figure out why the newlines are not removed when we remove an entire switch entry...
         final SwitchEntry defaultEntry = valueOfSwitch.getDefaultSwitchEntry().get();
