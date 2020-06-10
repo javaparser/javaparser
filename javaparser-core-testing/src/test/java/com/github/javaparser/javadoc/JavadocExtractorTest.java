d15e3ff4bdf6346c84dab69f0650a33bb90b13ff
Roger Howell <howelrtc@aston.ac.uk>
2020-06-10 15:55:31 +0100

    added an assertion and fixme comment to the javadoc extractor test

--- javaparser-core-testing/src/test/java/com/github/javaparser/javadoc/JavadocExtractorTest.java
+++ javaparser-core-testing/src/test/java/com/github/javaparser/javadoc/JavadocExtractorTest.java
@@ -22,36 +22,52 @@
 package com.github.javaparser.javadoc;
 
 import com.github.javaparser.ParseProblemException;
+import com.github.javaparser.ParserConfiguration;
+import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.comments.JavadocComment;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.charset.StandardCharsets;
 
-import static com.github.javaparser.StaticJavaParser.parse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JavadocExtractorTest {
 
+    private int errorCount;
+
+    @BeforeEach
+    void setUp() {
+        errorCount = 0;
+    }
+
     @Test
     void canParseAllJavadocsInJavaParser() throws FileNotFoundException {
+        System.err.println("About to deliberately parse a directory of files where some files have unsupported text encodings - ignore encoding-related errors that appear within the console.");
         processDir(new File(".."));
+        assertEquals(2, errorCount, " ");
     }
 
     private void processFile(File file) throws FileNotFoundException {
         try {
-            CompilationUnit cu = parse(file);
+            // Using (minimum) of Java 9 to avoid errors relating to parsing module-info.java files.
+            StaticJavaParser.getConfiguration().setLanguageLevel(ParserConfiguration.LanguageLevel.JAVA_9);
+            CompilationUnit cu = StaticJavaParser.parse(file);
             new VoidVisitorAdapter<Object>() {
                 @Override
                 public void visit(JavadocComment n, Object arg) {
                     super.visit(n, arg);
+                    // FIXME: The parse problems should probably be caught/examined here, where they would specifically be about parsing javadoc rather than parsing the file itself.
                     n.parse();
                 }
             }.visit(cu, null);
         } catch (ParseProblemException e) {
             System.err.println("ERROR PROCESSING "+ file);
+            errorCount++;
         }
     }
 
