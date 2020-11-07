f12876c5dfd07060ebc35d00c443b3eced07b81d
jlerbsc <5850581+jlerbsc@users.noreply.github.com>
2020-11-07 09:46:44 +0100

    Add test case fro issue 2823 Get UnsolvedSymbolException when trying to resolve type

--- javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/Issue2823Test.java
+++ javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/Issue2823Test.java
@@ -8,7 +8,6 @@ import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.expr.FieldAccessExpr;
-import com.github.javaparser.resolution.UnsolvedSymbolException;
 import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
@@ -17,7 +16,7 @@ import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeS
 public class Issue2823Test extends AbstractSymbolResolutionTest {
 
     @Test
-    public void testOK() {
+    public void test() {
         final Path testRoot = adaptPath("src/test/resources/issue2823");
         TypeSolver reflectionTypeSolver = new ReflectionTypeSolver();
         JavaParserTypeSolver javaParserTypeSolver = new JavaParserTypeSolver(testRoot);
@@ -26,53 +25,20 @@ public class Issue2823Test extends AbstractSymbolResolutionTest {
                 .setSymbolResolver(new JavaSymbolSolver(combinedTypeSolver));
         StaticJavaParser.setConfiguration(configuration);
 
-        String src = 
-                "import java.util.Optional;\n" 
-                + "public class TestClass {\n" 
+        String src = "import java.util.Optional;\n"
+                + "public class TestClass {\n"
                 + "    public Long getValue() {\n"
                 + "        Optional<ClassA> classA = Optional.of(new ClassA());\n"
-                + "        return classA.map(a -> a.obj)\n" 
+                + "        return classA.map(a -> a.obj)\n"
                 + "                .map(b -> b.value)\n"
-                + "                .orElse(null);\n" 
-                + "    }\n" 
+                + "                .orElse(null);\n"
+                + "    }\n"
                 + "}";
 
         CompilationUnit cu = StaticJavaParser.parse(src);
 
-        try {
-            cu.findAll(FieldAccessExpr.class).forEach(FieldAccessExpr::resolve);
-        } catch (UnsolvedSymbolException e) {
-            // Bug to be solved
-        } 
-
-    }
-
-    @Test
-    public void testKO() {
-        final Path testRoot = adaptPath("src/test/resources/issue2823");
-        TypeSolver reflectionTypeSolver = new ReflectionTypeSolver();
-        JavaParserTypeSolver javaParserTypeSolver = new JavaParserTypeSolver(testRoot);
-        CombinedTypeSolver combinedTypeSolver = new CombinedTypeSolver(reflectionTypeSolver, javaParserTypeSolver);
-        ParserConfiguration configuration = new ParserConfiguration()
-                .setSymbolResolver(new JavaSymbolSolver(combinedTypeSolver));
-        StaticJavaParser.setConfiguration(configuration);
-
-        String src = 
-                "import java.util.Optional;\n" 
-                + "import ClassA;\n" + "import ClassB;\n"
-                + "public class TestClass {\n" 
-                + "    public Long getValue() {\n"
-                + "        Optional<ClassA> classA = Optional.of(new ClassA());\n"
-                + "        Optional<ClassB> classB = classA.map(a -> a.obj);\n" 
-                + "        return classB\n"
-                + "                .map(b -> b.value)\n" 
-                + "                .orElse(null);" 
-                + "    }\n" 
-                + "}";
-
-        CompilationUnit cu = StaticJavaParser.parse(src);
-
-        cu.findAll(FieldAccessExpr.class).forEach(FieldAccessExpr::resolve);
+        // verify there is no exception thrown when we try to resolve all field access expressions
+        cu.findAll(FieldAccessExpr.class).forEach(fd -> fd.resolve());
 
     }
 }
