7798ddc6c9d98c47f3af318f0d449c9d6f4c88b0
Roger Howell <howelrtc@aston.ac.uk>
2020-11-12 20:28:03 +0000

    copy test cases from #2907

--- javaparser-core-testing/src/test/java/com/github/javaparser/ast/expr/BinaryExprTest.java
+++ javaparser-core-testing/src/test/java/com/github/javaparser/ast/expr/BinaryExprTest.java
@@ -21,13 +21,119 @@
 
 package com.github.javaparser.ast.expr;
 
+import com.github.javaparser.StaticJavaParser;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BinaryExprTest {
+
     @Test
     void convertOperator() {
         assertEquals(AssignExpr.Operator.PLUS, BinaryExpr.Operator.PLUS.toAssignOperator().get());
+    }
+
+    /**
+     * Evaluation takes place left to right, with && taking precedence over ||
+     *
+     * true || false && false || false
+     * true ||      (1)       || false
+     * (        2           ) || false
+     * (             3               )
+     *
+     * true || false && false || false
+     * true ||    (false)     || false
+     * (     true           ) || false
+     * (           true              )
+     */
+    @Nested
+    class LogicalOperatorPrecedence {
+
+        @Test
+        public void logicalAndOr() {
+            Expression expression = StaticJavaParser.parseExpression("true || false && false || false");
+            Expression bracketedExpression = applyBrackets(expression);
+
+            String expected = "(true || (false && false)) || false";
+            String actual = bracketedExpression.toString();
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void logicalOrEvaluationLeftToRight() {
+            Expression expression = StaticJavaParser.parseExpression("false || true || false || true || false || true");
+            Expression bracketedExpression = applyBrackets(expression);
+
+            String expected = "((((false || true) || false) || true) || false) || true";
+            String actual = bracketedExpression.toString();
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void logicalAndEvaluationLeftToRight() {
+            Expression expression = StaticJavaParser.parseExpression("false && true && false && true && false && true");
+            Expression bracketedExpression = applyBrackets(expression);
+
+            String expected = "((((false && true) && false) && true) && false) && true";
+            String actual = bracketedExpression.toString();
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void andTakesPrecedenceOverOr() {
+            Expression expression = StaticJavaParser.parseExpression("true || false && false");
+            Expression bracketedExpression = applyBrackets(expression);
+
+            String expected = "true || (false && false)";
+            String actual = bracketedExpression.toString();
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void andTakesPrecedenceOverOrThenLeftToRight() {
+            Expression expression = StaticJavaParser.parseExpression("true || false && false || true");
+            Expression bracketedExpression = applyBrackets(expression);
+
+            String expected = "(true || (false && false)) || true";
+            String actual = bracketedExpression.toString();
+
+            assertEquals(expected, actual);
+        }
+
+
+        @Test
+        public void example() {
+            Expression expression = StaticJavaParser.parseExpression("year % 4 == 0 && year % 100 != 0 || year % 400 == 0");
+            Expression bracketedExpression = applyBrackets(expression);
+
+            String expected = "((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)";
+            String actual = bracketedExpression.toString();
+
+            assertEquals(expected, actual);
+        }
+
+
+    }
+
+
+    private Expression applyBrackets(Expression expression) {
+        expression.findAll(BinaryExpr.class)
+                .stream()
+                .filter(binaryExpr -> binaryExpr.getOperator() == BinaryExpr.Operator.AND || binaryExpr.getOperator() == BinaryExpr.Operator.OR)
+                .forEach(binaryExpr -> {
+                    if(!binaryExpr.getLeft().isBooleanLiteralExpr()) {
+                        binaryExpr.setLeft(new EnclosedExpr(binaryExpr.getLeft()));
+                    }
+                    if(!binaryExpr.getRight().isBooleanLiteralExpr()) {
+                        binaryExpr.setRight(new EnclosedExpr(binaryExpr.getRight()));
+                    }
+                });
+
+        return expression;
     }
 }
