aaee75d47682f8378a4d869b38d2c0978449f710
Maarten Coene <4728619+maartenc@users.noreply.github.com>
2021-03-15 10:35:55 +0100

    Fix test

--- javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/javassistmodel/JavassistInterfaceDeclarationTest.java
+++ javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/javassistmodel/JavassistInterfaceDeclarationTest.java
@@ -35,6 +35,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -156,6 +157,12 @@ class JavassistInterfaceDeclarationTest extends AbstractSymbolResolutionTest {
     void testGetAncestorsWithGenericAncestors() {
         JavassistInterfaceDeclaration compilationUnit = (JavassistInterfaceDeclaration) anotherTypeSolver.solveType("com.github.javaparser.test.GenericChildInterface");
         List<ResolvedReferenceType> ancestors = compilationUnit.getAncestors();
+        ancestors.sort(new Comparator<ResolvedReferenceType>() {
+            @Override
+            public int compare(ResolvedReferenceType o1, ResolvedReferenceType o2) {
+                return o1.describe().compareTo(o2.describe());
+            }
+        });
         assertEquals(2, ancestors.size());
         assertEquals("com.github.javaparser.test.GenericInterface<S>", ancestors.get(0).describe()); // Type should be 'S', from the GenericChildInterface
         assertEquals("java.lang.Object", ancestors.get(1).describe());
