f85c202618a0d29187e1662525551d652fc2d209
Roger Howell <MysterAitch@users.noreply.github.com>
2021-04-06 23:52:19 +0100

    fixed ellipsis and doublecolon to be categorised as separators not operators (fixes #2897), run generators, and added test to ensure that GeneratedJavaParserConstants and JavaToken stay synchronised / up to date

--- javaparser-core/src/main/java/com/github/javaparser/TokenTypes.java
+++ javaparser-core/src/main/java/com/github/javaparser/TokenTypes.java
@@ -93,7 +93,6 @@ public class TokenTypes {
     /**
      * Category of a token, a little more detailed than
      * <a href="https://docs.oracle.com/javase/specs/jls/se8/html/jls-3.html#jls-3.5">The JLS</a>.
-     * FIXME: It appears that {@code ...} {@code ELLIPSIS} and {@code ::} {@code DOUBLECOLON} are (wrongly) listed in the "operators" section, rather than "separators"
      */
     public static JavaToken.Category getCategory(int kind) {
         switch (kind) {
@@ -201,7 +200,9 @@ public class TokenTypes {
             case SEMICOLON:
             case COMMA:
             case DOT:
+            case ELLIPSIS:
             case AT:
+            case DOUBLECOLON:
                 return JavaToken.Category.SEPARATOR;
             case ASSIGN:
             case LT:
@@ -237,9 +238,7 @@ public class TokenTypes {
             case LSHIFTASSIGN:
             case RSIGNEDSHIFTASSIGN:
             case RUNSIGNEDSHIFTASSIGN:
-            case ELLIPSIS:
             case ARROW:
-            case DOUBLECOLON:
             case RUNSIGNEDSHIFT:
             case RSIGNEDSHIFT:
             case GT:
