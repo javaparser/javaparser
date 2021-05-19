2713dffee444dbaa4e10aa47ec2bcaf7448dec04
Maarten Coene <4728619+maartenc@users.noreply.github.com>
2021-05-19 10:42:33 +0200

    Revert "Use List instead of Set to avoid slow hashCode calculations on big Nodes"
    This reverts commit 06495fa9

--- javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/reflectionmodel/ReflectionEnumDeclarationTest.java
+++ javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/reflectionmodel/ReflectionEnumDeclarationTest.java
@@ -124,7 +124,7 @@ class ReflectionEnumDeclarationTest extends AbstractSymbolResolutionTest {
     @Test
     void testInternalTypesEmpty() {
         ReflectionEnumDeclaration modifier = (ReflectionEnumDeclaration) typeSolver.solveType("com.github.javaparser.symbolsolver.reflectionmodel.MyModifier");
-        assertEquals(Collections.emptyList(), modifier.internalTypes());
+        assertEquals(Collections.emptySet(), modifier.internalTypes());
     }
 
 }
