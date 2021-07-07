c676df72e73e2795b4c41d36e2f6065929caaa52
Roger Howell <MysterAitch@users.noreply.github.com>
2021-07-07 01:54:21 +0100

    Fix #3308 - Access the parent context (NOT the parent node) when solving in the context of ArrayAccessExpr nodes.

--- javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/Issue3308Test.java
+++ javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/Issue3308Test.java
@@ -56,4 +56,47 @@ public class Issue3308Test {
         });
     }
 
+
+
+
+    @Test
+    void test2() {
+        StaticJavaParser.getConfiguration().setLanguageLevel(ParserConfiguration.LanguageLevel.JAVA_9);
+        CombinedTypeSolver combinedTypeSolver = new CombinedTypeSolver();
+        combinedTypeSolver.add(new ReflectionTypeSolver());
+        StaticJavaParser.getConfiguration().setSymbolResolver(new JavaSymbolSolver(combinedTypeSolver));
+        String classStr = "class JavaParser {\n" +
+                "\n" +
+                "    public void bad (int index) {\n" +
+                "        LastRecovered recovered = new LastRecovered();\n" +
+                "        recovered.perPriority[index][0][0][0].recovered = 10;\n" +
+                "    }\n" +
+                "\n" +
+                "    private class LastRecovered {\n" +
+                "        LastRecoveredEntry[][][][] perPriority = new LastRecoveredEntry[10][10][10][10];\n" +
+                "    }\n" +
+                "\n" +
+                "    private class LastRecoveredEntry {\n" +
+                "        private int recovered = 0;\n" +
+                "    }\n" +
+                "}";
+
+        CompilationUnit node = StaticJavaParser.parse(classStr);
+        List<ArrayAccessExpr> all = node.findAll(ArrayAccessExpr.class);
+//        List<NameExpr> all = node.findAll(NameExpr.class);
+//        List<FieldAccessExpr> all = node.findAll(FieldAccessExpr.class);
+        all.remove(0);
+        all.forEach(a -> {
+            System.out.println("a = " + a);
+            System.out.println("a.getName() = " + a.getName());
+//            try {
+//                ResolvedValueDeclaration resolved = a.resolve();
+//                System.out.println("resolved = " + resolved);
+//            } catch (Exception e) {
+//                System.out.println("e = " + e);
+//            }
+            System.out.println();
+        });
+    }
+
 }
