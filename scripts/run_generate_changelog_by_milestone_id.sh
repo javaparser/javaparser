94cf0128ecee631615b95f5b4721432d8cdb7cc8
Roger Howell <MysterAitch@users.noreply.github.com>
2021-10-05 11:26:08 +0100

    refactor / tidy up

--- scripts/run_generate_changelog_by_milestone_id.sh
+++ scripts/run_generate_changelog_by_milestone_id.sh
@@ -6,11 +6,24 @@
 ##
 ## This fork is available at https://github.com/MysterAitch/github-changelog-generator
 ##
-## For clarity the jar is name to explicitly indicate that this is the forked version not the original.
+## For clarity, the jar is named to explicitly indicate that this is the forked version not the original.
 ##
+
+
+## Exit if number of given parameters is incorrect (expecting 1 - the milestone id)
+if [ "$#" -ne 1 ]; then
+  echo "[JavaParser-ERROR]: No arguments supplied. Exiting." >&2
+  echo "[JavaParser-ERROR]: Expected Usage: $0 MILESTONE_ID" >&2
+  exit 3
+fi
+
+## Variables
+CHANGELOG_ID=$1
 
 #CHANGELOG_GENERATOR_JAR=./github-changelog-generator.jar
 CHANGELOG_GENERATOR_JAR=./github-changelog-generator_mysteraitch.jar
+CONFIG_FILE_LOCATION="release-notes-config-id.yml"
+OUTPUT_FILE=temp_changelog.md ## TODO: Consider making the output file configurable.
 
 
 echo "[JavaParser]"
@@ -21,19 +34,6 @@ echo "[JavaParser]: Expected Usage: $0 MILESTONE_ID"
 echo "[JavaParser]: Where the MILESTONE_ID is the number within the milestone URL"
 echo "[JavaParser]:     e.g. $0 182"
 #echo "[JavaParser]: e.g. https://github.com/javaparser/javaparser/milestone/182"
-
-
-## Exit if
-if [ "$#" -ne 1 ]; then
-  echo "[JavaParser-ERROR]: No arguments supplied. Exiting." >&2
-  echo "[JavaParser-ERROR]: Expected Usage: $0 MILESTONE_ID" >&2
-  exit 1
-fi
-
-## Variables -- TODO: Consider making the output file configurable.
-CHANGELOG_ID=$1
-OUTPUT_FILE=temp_changelog.md
-
 echo "[JavaParser]"
 echo "[JavaParser]: - A changelog specific to that milestone will then be output to $OUTPUT_FILE"
 echo "[JavaParser]: - A changelog specific to that milestone will then be output to the console"
@@ -43,32 +43,28 @@ echo "[JavaParser]: About to clear $OUTPUT_FILE ready for populating."
 echo "[JavaParser]"
 set -x
 ## Empty out the changelog:
+ # shellcheck disable=SC2188
  > $OUTPUT_FILE
 
 ## Run the changelog generator tool, to generate a changelog.
-java -jar $CHANGELOG_GENERATOR_JAR --spring.config.location="release-notes-config-id.yml" "$CHANGELOG_ID" $OUTPUT_FILE
+java -jar $CHANGELOG_GENERATOR_JAR --spring.config.location="$CONFIG_FILE_LOCATION" "$CHANGELOG_ID" $OUTPUT_FILE
 CHANGELOG_GENERATOR_EXIT_CODE=$?
-#echo $CHANGELOG_GENERATOR_EXIT_CODE
 set +x
 
 ## If the generator failed (e.g. due to an invalid milestone reference), exit this script.
 if [ $CHANGELOG_GENERATOR_EXIT_CODE -ne 0 ]; then
   echo "[JavaParser-ERROR]: Generation of the changelog failed. Exiting." >&2
-  echo "[JavaParser-ERROR]: Exiting with code: $CHANGELOG_GENERATOR_EXIT_CODE" >&2
-  exit 3
+  exit 4
 fi
 
 
-## Also output the generated changelog to the console.
+## Also display the generated changelog to the console.
+CHANGELOG_CONTENTS=$(<$OUTPUT_FILE)
+
 echo "[JavaParser]"
 echo "[JavaParser]"
 echo "[JavaParser]"
 echo "[JavaParser]: The changelog for milestone ID $CHANGELOG_ID (also stored within $OUTPUT_FILE)"
 echo "[JavaParser]"
 echo ""
-
-
-## Get contents of the
-value=$(<$OUTPUT_FILE)
-echo "$value"
-
+echo "$CHANGELOG_CONTENTS"
