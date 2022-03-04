c92ff2adeb758514571e9cb8aae34795964d4982
Alexander Weigl <weigl@kit.edu>
2022-03-04 12:10:05 +0100

    fix errors

--- javaparser-key-testing/src/test/java/com/github/javaparser/ast/key/sv/SchemaJavaBulkTest.java
+++ javaparser-key-testing/src/test/java/com/github/javaparser/ast/key/sv/SchemaJavaBulkTest.java
@@ -4,6 +4,7 @@ import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseResult;
 import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.Problem;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -13,6 +14,7 @@ class SchemaJavaBulkTest {
     @ParameterizedTest
     @CsvFileSource(resources = "/com/github/javaparser/schemajava.txt", delimiter = 'ä')
     void testSchemaJava(String input) {
+        Assumptions.assumeFalse(input.trim().startsWith("#"));
         //System.err.println(input);
         ParserConfiguration config = new ParserConfiguration();
         config.setPreprocessUnicodeEscapes(true);
