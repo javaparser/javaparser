847fd15f2adebfcd1f3c0e88f739885ec1ca0b76
Alexander Weigl <weigl@kit.edu>
2022-06-14 18:42:40 +0200

    Jml annotation translator

---
+++ javaparser-core/src/main/java/com/github/javaparser/jml/impl/JmlAnnotationConfiguration.java
@@ -0,0 +1,27 @@
+package com.github.javaparser.jml.impl;
+
+import com.github.javaparser.ast.Modifier;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JmlAnnotationConfiguration {
+    public static final String NOT_NULL = "org.jetbrains.annotations.NotNull";
+    public static final String NULLABLE = "org.jetbrains.annotations.Nullable";
+    public static final String CONTRACT = "org.jetbrains.annotations.Contract";
+
+    private final Map<String, Modifier.DefaultKeyword> annotationToModifier = new HashMap<>();
+
+    public Map<String, Modifier.DefaultKeyword> getAnnotationToModifier() {
+        return annotationToModifier;
+    }
+
+    public static JmlAnnotationConfiguration createDefault() {
+        JmlAnnotationConfiguration c = new JmlAnnotationConfiguration();
+        c.annotationToModifier.put(NOT_NULL, Modifier.DefaultKeyword.JML_NON_NULL);
+        c.annotationToModifier.put(NULLABLE, Modifier.DefaultKeyword.JML_NULLABLE);
+        c.annotationToModifier.put("javax.validation.constraints.NotNull", Modifier.DefaultKeyword.JML_NON_NULL);
+        c.annotationToModifier.put("javax.validation.constraints.Null", Modifier.DefaultKeyword.JML_NULLABLE);
+        return c;
+    }
+}
---
+++ javaparser-core/src/main/java/com/github/javaparser/jml/impl/JmlAnnotationSupport.java
@@ -0,0 +1,67 @@
+package com.github.javaparser.jml.impl;
+
+import com.github.javaparser.*;
+import com.github.javaparser.ast.Modifier;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.AnnotationExpr;
+import com.github.javaparser.ast.nodeTypes.NodeWithAnnotations;
+import com.github.javaparser.ast.nodeTypes.NodeWithModifiers;
+import com.github.javaparser.ast.validator.ProblemReporter;
+import com.github.javaparser.ast.visitor.TreeVisitor;
+import com.github.javaparser.resolution.types.ResolvedType;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Alexander Weigl
+ * @version 1 (4/10/20)
+ */
+public class JmlAnnotationSupport extends Processor {
+    public final JmlAnnotationConfiguration configuration;
+
+    public JmlAnnotationSupport(JmlAnnotationConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    public void postProcess(ParseResult<? extends Node> result, ParserConfiguration configuration) {
+        if (result.isSuccessful()) {
+            Map<String, Modifier.DefaultKeyword> a2m = JmlAnnotationSupport.this.configuration.getAnnotationToModifier();
+            TreeVisitor visitor = new JmlAnnotationTranslator(a2m, result.getProblems());
+            result.getResult().ifPresent(visitor::visitPreOrder);
+        }
+    }
+}
+
+class JmlAnnotationTranslator extends TreeVisitor {
+    private final Map<String, Modifier.DefaultKeyword> a2m;
+    private final ProblemReporter problemReport;
+
+    public JmlAnnotationTranslator(Map<String, Modifier.DefaultKeyword> a2m, List<Problem> problems) {
+        this.a2m = a2m;
+        problemReport = new ProblemReporter(problems::add);
+    }
+
+    @Override
+    public void process(Node node) {
+        if (node instanceof NodeWithAnnotations<?>) {
+            NodeWithAnnotations<?> n = (NodeWithAnnotations<?>) node;
+            for (AnnotationExpr annotation : n.getAnnotations()) {
+                ResolvedType type = annotation.calculateResolvedType();
+                String fqn = type.asReferenceType().getQualifiedName();
+                if (a2m.containsKey(fqn)) {
+                    try {
+                        NodeWithModifiers<?> m = (NodeWithModifiers<?>) node;
+                        m.addModifier(a2m.get(fqn));
+                    } catch (ClassCastException e) {
+                        problemReport.report(annotation,
+                                "Could not translate annotation %s into a modifier. " +
+                                        "Target node '%s' does support modifiers.", annotation.getName(),
+                                node.getMetaModel().getTypeName());
+                    }
+                }
+            }
+        }
+    }
+}
