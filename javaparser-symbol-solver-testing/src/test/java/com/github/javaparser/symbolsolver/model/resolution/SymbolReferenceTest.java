e19d4e9e4c5e1bbdcfc58e848e5c25e0f92ecf38
4everTheOne <j.a.h.oliveira@hotmail.com>
2022-10-30 01:35:52 +0100

    Added unit tests for class

---
+++ javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/model/resolution/SymbolReferenceTest.java
@@ -0,0 +1,68 @@
+package com.github.javaparser.symbolsolver.model.resolution;
+
+import com.github.javaparser.resolution.declarations.ResolvedClassDeclaration;
+import com.github.javaparser.resolution.declarations.ResolvedDeclaration;
+import com.github.javaparser.resolution.declarations.ResolvedParameterDeclaration;
+import com.github.javaparser.symbolsolver.reflectionmodel.ReflectionClassDeclaration;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SymbolReferenceTest {
+
+    private final TypeSolver typeSolver = new ReflectionTypeSolver();
+
+    @Test
+    void testResolvedSymbol() {
+        ResolvedDeclaration resolvedDeclaration = new ReflectionClassDeclaration(String.class, typeSolver);
+        SymbolReference<ResolvedDeclaration> symbol = SymbolReference.solved(resolvedDeclaration);
+
+        assertNotNull(symbol);
+        assertNotNull(symbol.getDeclaration());
+        assertTrue(symbol.getDeclaration().isPresent());
+    }
+
+    @Test
+    void testUnresolvedSymbol() {
+        SymbolReference<ResolvedDeclaration> symbol = SymbolReference.unsolved();
+
+        assertNotNull(symbol);
+        assertNotNull(symbol.getDeclaration());
+        assertFalse(symbol.getDeclaration().isPresent());
+    }
+
+    @Test
+    void testAdaptSymbolForSubClass() {
+        ResolvedDeclaration resolvedDeclaration = new ReflectionClassDeclaration(String.class, typeSolver);
+        SymbolReference<ResolvedDeclaration> symbol = SymbolReference.solved(resolvedDeclaration);
+        SymbolReference<ResolvedClassDeclaration> adaptedSymbol = SymbolReference.adapt(symbol, ResolvedClassDeclaration.class);
+
+        assertNotNull(adaptedSymbol);
+        assertNotNull(adaptedSymbol.getDeclaration());
+        assertTrue(adaptedSymbol.getDeclaration().isPresent());
+    }
+
+    @Test
+    void testAdaptSymbolForInvalidSubClass() {
+        ResolvedClassDeclaration resolvedDeclaration = new ReflectionClassDeclaration(String.class, typeSolver);
+        SymbolReference<ResolvedClassDeclaration> symbol = SymbolReference.solved(resolvedDeclaration);
+        SymbolReference<ResolvedParameterDeclaration> adaptedSymbol = SymbolReference.adapt(symbol, ResolvedParameterDeclaration.class);
+
+        assertNotNull(adaptedSymbol);
+        assertNotNull(adaptedSymbol.getDeclaration());
+        assertFalse(adaptedSymbol.getDeclaration().isPresent());
+    }
+
+    @Test
+    void testAdaptSymbolForSuperClass() {
+        ResolvedClassDeclaration resolvedDeclaration = new ReflectionClassDeclaration(String.class, typeSolver);
+        SymbolReference<ResolvedClassDeclaration> symbol = SymbolReference.solved(resolvedDeclaration);
+        SymbolReference<ResolvedDeclaration> adaptedSymbol = SymbolReference.adapt(symbol, ResolvedDeclaration.class);
+
+        assertNotNull(adaptedSymbol);
+        assertNotNull(adaptedSymbol.getDeclaration());
+        assertTrue(adaptedSymbol.getDeclaration().isPresent());
+    }
+
+}
