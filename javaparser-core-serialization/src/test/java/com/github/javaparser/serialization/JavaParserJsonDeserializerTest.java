db3320fe1356b3d7a8bd5d30072f16b49cc79647
4everTheOne <j.a.h.oliveira@hotmail.com>
2022-12-16 20:30:09 +0000

    Organized Imports for JavaParser-Core-Serialization

--- javaparser-core-serialization/src/test/java/com/github/javaparser/serialization/JavaParserJsonDeserializerTest.java
+++ javaparser-core-serialization/src/test/java/com/github/javaparser/serialization/JavaParserJsonDeserializerTest.java
@@ -20,7 +20,10 @@
  */
 package com.github.javaparser.serialization;
 
-import com.github.javaparser.*;
+import com.github.javaparser.ParserConfiguration;
+import com.github.javaparser.Range;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.TokenRange;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
