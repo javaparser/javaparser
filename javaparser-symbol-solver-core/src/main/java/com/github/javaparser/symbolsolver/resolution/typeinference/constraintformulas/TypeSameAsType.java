fe328c7c94a52108b3470b146b35d1370c67f7a2
Felix Qvarfordt <felixq@kth.se>
2023-02-19 23:46:20 +0100

    (feat) #12 added branch coverage tool for reduce method
    Added a simple branch coverage tool for the reduce method in the
    TypeSameAsType class in the symbolsolver project.
    
    Also added Jacoco to this part to be able to compare results with
    another tool.

--- javaparser-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/resolution/typeinference/constraintformulas/TypeSameAsType.java
+++ javaparser-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/resolution/typeinference/constraintformulas/TypeSameAsType.java
@@ -39,66 +39,100 @@ public class TypeSameAsType extends ConstraintFormula {
     private ResolvedType S;
     private ResolvedType T;
 
+    public static boolean[] reduceBranchesReached = new boolean[19];
+
     public TypeSameAsType(ResolvedType s, ResolvedType t) {
         S = s;
         T = t;
     }
 
+    public static void printReachedBranches(){
+        for (int i = 1; i < 19; i++)
+            System.out.print("Reduce Branch " + i + (reduceBranchesReached[i] ? "Reached" : "not Reached"));
+    }
+
     @Override
     public ReductionResult reduce(BoundSet currentBoundSet) {
         // A constraint formula of the form ‹S = T›, where S and T are types, is reduced as follows:
-
+        //id 1
+        reduceBranchesReached[1] = !S.isWildcard() || reduceBranchesReached[1];
         if (!S.isWildcard() && !T.isWildcard()) {
-
+            //id 2
+            reduceBranchesReached[2] = true;
             // - If S and T are proper types, the constraint reduces to true if S is the same as T (§4.3.4), and false
             //   otherwise.
-
+            //id 3
+            reduceBranchesReached[3] = isProperType(S) || reduceBranchesReached[3];
             if (isProperType(S) && isProperType(T)) {
+                //id 4
+                reduceBranchesReached[4] = true;
                 if (S.equals(T)) {
+                    //id 5
+                    reduceBranchesReached[5] = true;
                     return ReductionResult.trueResult();
                 } else {
+                    //id 6
+                    reduceBranchesReached[6] = true;
                     return ReductionResult.falseResult();
                 }
             }
 
             // - Otherwise, if S or T is the null type, the constraint reduces to false.
-
+            //id 7
+            reduceBranchesReached[7] = S.isNull();
             if (S.isNull() || T.isNull()) {
+                //id 8
+                reduceBranchesReached[8] = true;
                 return ReductionResult.falseResult();
             }
 
             // - Otherwise, if S is an inference variable, α, and T is not a primitive type, the constraint reduces to the
             //   bound α = T.
-
+            //id 9
+            reduceBranchesReached[9] = S.isInferenceVariable() || reduceBranchesReached[9];
             if (S.isInferenceVariable() && !T.isPrimitive()) {
+                //id 10
+                reduceBranchesReached[10] = true;
                 return ReductionResult.oneBound(new SameAsBound(S, T));
             }
 
             // - Otherwise, if T is an inference variable, α, and S is not a primitive type, the constraint reduces to the
             //   bound S = α.
-
+            //id 11
+            reduceBranchesReached[11] = T.isInferenceVariable() || reduceBranchesReached[9];
             if (T.isInferenceVariable() && !S.isPrimitive()) {
+                //id 12
+                reduceBranchesReached[12] = true;
                 return ReductionResult.oneBound(new SameAsBound(S, T));
             }
 
             // - Otherwise, if S and T are class or interface types with the same erasure, where S has
             //   type arguments B1, ..., Bn and T has type arguments A1, ..., An, the constraint reduces to the following
             //   new constraints: for all i (1 ≤ i ≤ n), ‹Bi = Ai›.
-
+            //id 13, 14
+            reduceBranchesReached[13] = S.isReferenceType() || reduceBranchesReached[13];
+            reduceBranchesReached[14] = T.isReferenceType() || reduceBranchesReached[14];
             if (S.isReferenceType() && T.isReferenceType()
                     && S.asReferenceType().toRawType().equals(T.asReferenceType().toRawType())) {
+                //id 15
+                reduceBranchesReached[15] = true;
                 ReductionResult res = ReductionResult.empty();
                 List<ResolvedType> Bs = S.asReferenceType().typeParametersValues();
                 List<ResolvedType> As = T.asReferenceType().typeParametersValues();
                 for (int i = 0; i < Bs.size(); i++) {
+                    //id 16
+                    reduceBranchesReached[16] = true;
                     res = res.withConstraint(new TypeSameAsType(Bs.get(i), As.get(i)));
                 }
                 return res;
             }
 
             // - Otherwise, if S and T are array types, S'[] and T'[], the constraint reduces to ‹S' = T'›.
-
+            //id 17
+            reduceBranchesReached[17] = S.isArray() || reduceBranchesReached[17];
             if (S.isArray() && T.isArray()) {
+                //id 18
+                reduceBranchesReached[18] = true;
                 return ReductionResult.oneConstraint(new TypeSameAsType(
                         S.asArrayType().getComponentType(),
                         T.asArrayType().getComponentType()));
