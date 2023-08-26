b4b7eb20e0001e2303744fa088094b9357a2f1d0
jlerbsc <jean-pierre.lerbscher@jperf.com>
2023-08-26 19:49:36 +0200

    Test method is only enabled for a specific range of Java RuntimeEnvironment (JRE) versions from java 9

--- javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/Issue3976Test.java
+++ javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/Issue3976Test.java
@@ -3,6 +3,8 @@ package com.github.javaparser.symbolsolver;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledForJreRange;
+import org.junit.jupiter.api.condition.JRE;
 
 import com.github.javaparser.JavaParserAdapter;
 import com.github.javaparser.ast.CompilationUnit;
@@ -13,15 +15,15 @@ import com.github.javaparser.symbolsolver.resolution.AbstractResolutionTest;
 public class Issue3976Test extends AbstractResolutionTest {
 
     @Test
+    @EnabledForJreRange(min = JRE.JAVA_9)
     void test() {
     	String testCase =
 				"import java.util.Map;\n"
-				+ "public class Foo{\n"
-				+ "	public Object m() {\n"
-				+ "		return Map.of(\"k0\", 0, \"k1\", 1D);\n"
-				+ "	}\n"
-				+ "}\n"
-				+ "";
+				+ "public class Foo {\n"
+				+ "		public Object m() {\n"
+				+ "			return Map.of(\"k0\", 0, \"k1\", 1D);\n"
+				+ "		}\n"
+				+ "}";
 
 		CompilationUnit cu = JavaParserAdapter.of(createParserWithResolver(defaultTypeSolver())).parse(testCase);
 
