bbc711c1c84f0899edb45eeb63221ea4cf3cad59
jlerbsc <jean-pierre.lerbscher@jperf.com>
2023-08-26 18:13:28 +0200

    Fix: issue 3976 Issue resolving implicit generic types

---
+++ javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/Issue3976Test.java
@@ -0,0 +1,33 @@
+package com.github.javaparser.symbolsolver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.github.javaparser.JavaParserAdapter;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.resolution.types.ResolvedType;
+import com.github.javaparser.symbolsolver.resolution.AbstractResolutionTest;
+
+public class Issue3976Test extends AbstractResolutionTest {
+
+    @Test
+    void test() {
+    	String testCase =
+				"import java.util.Map;\n"
+				+ "public class Foo{\n"
+				+ "	public Object m() {\n"
+				+ "		return Map.of(\"k0\", 0, \"k1\", 1D);\n"
+				+ "	}\n"
+				+ "}\n"
+				+ "";
+
+		CompilationUnit cu = JavaParserAdapter.of(createParserWithResolver(defaultTypeSolver())).parse(testCase);
+
+		MethodCallExpr methodCallExpr = cu.findFirst(MethodCallExpr.class).get();
+
+		ResolvedType rt = methodCallExpr.calculateResolvedType();
+		assertEquals("java.util.Map<java.lang.String, java.lang.Number>", rt.describe());
+    }
+}
