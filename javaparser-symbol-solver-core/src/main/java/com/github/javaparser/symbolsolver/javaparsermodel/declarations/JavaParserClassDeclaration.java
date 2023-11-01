a4f19c74a64d3b255066c855964d2b656daa790a
Ruil1n <ruilin911@outlook.com>
2023-11-01 07:56:22 +0000

    do not consider same qualified name IN isAncestor FUNCTION

--- javaparser-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/javaparsermodel/declarations/JavaParserClassDeclaration.java
+++ javaparser-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/javaparsermodel/declarations/JavaParserClassDeclaration.java
@@ -392,8 +392,8 @@ public class JavaParserClassDeclaration extends AbstractClassDeclaration
         Optional<ResolvedReferenceTypeDeclaration> resolvedReferenceTypeDeclaration = candidateAncestor.getTypeDeclaration();
         if (resolvedReferenceTypeDeclaration.isPresent()) {
             ResolvedTypeDeclaration rtd = resolvedReferenceTypeDeclaration.get().asType();
-            // do not consider an inner or nested class as an ancestor
-            return !rtd.hasInternalType(ownQualifiedName);
+            // do not consider an inner or nested class as an ancestor, do not consider same qualified name
+            return !rtd.hasInternalType(ownQualifiedName) && !ownQualifiedName.equals(rtd.getQualifiedName());
         }
         return false;
     }
