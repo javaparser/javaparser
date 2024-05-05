0309e917e88427f4bdf14a09e917c7e517d709b9
Matthieu Vergne <matthieu.vergne@gmail.com>
2024-05-05 22:21:26 +0200

    Fixes unchecked warnings when calling Mockito.mock(Class)
    This is an issue well known by the Mockito community, but prone to not
    be fixed:
    https://github.com/mockito/mockito/issues/1531
    
    Generics allow to ensure type-safety at compile time instead of runtime.
    However, here it is used mainly to auto-cast the created object, thus
    avoiding this burden on the developer. Indeed, it is in a context where
    type safety is usually not a requirement, since the use of this method
    is often the most trivial we can have: provide a class and expect a very
    instance of this class in return.
    
    This commit thus creates a less constrained mock method which builds on
    Mockito's one, but without the constraint inducing this warning.

--- javaparser-core-testing/src/test/java/com/github/javaparser/ast/visitor/GenericVisitorAdapterTest.java
+++ javaparser-core-testing/src/test/java/com/github/javaparser/ast/visitor/GenericVisitorAdapterTest.java
@@ -37,7 +37,6 @@ import org.mockito.Mockito;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 
 public class GenericVisitorAdapterTest {
@@ -2549,4 +2548,10 @@ public class GenericVisitorAdapterTest {
         order.verifyNoMoreInteractions();
     }
 
+	@SuppressWarnings("unchecked")
+	// Non type-safe mock method to avoid unchecked warnings
+	// Its use is trivial and systematic enough to not be a problem
+	private <T> T mock(Class<?> classToMock) {
+		return (T) Mockito.mock(classToMock);
+	}
 }
