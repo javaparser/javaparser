181b471cff83e69f3d1b1428b726e5875575ffe1
Johannes Coetzee <jcoetzee@qwiet.ai>
2024-05-23 18:23:23 +0200

    Add validators and tests for record patterns

--- javaparser-core-testing/src/test/java/com/github/javaparser/ast/validator/Java21ValidatorTest.java
+++ javaparser-core-testing/src/test/java/com/github/javaparser/ast/validator/Java21ValidatorTest.java
@@ -25,13 +25,15 @@ import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseResult;
 import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.stmt.Statement;
+import com.github.javaparser.utils.TestUtils;
 import org.junit.jupiter.api.Test;
 
 import static com.github.javaparser.ParseStart.EXPRESSION;
+import static com.github.javaparser.ParseStart.STATEMENT;
 import static com.github.javaparser.ParserConfiguration.LanguageLevel.JAVA_21;
 import static com.github.javaparser.Providers.provider;
 import static com.github.javaparser.utils.TestUtils.assertNoProblems;
-import static com.github.javaparser.utils.TestUtils.assertProblems;
 
 class Java21ValidatorTest {
 
@@ -47,6 +49,12 @@ class Java21ValidatorTest {
     void switchPatternWithGuardAllowed() {
         ParseResult<Expression> result = javaParser.parse(EXPRESSION, provider("switch(x){case String s when s.length() > 5 -> System.out.println(0);}"));
         assertNoProblems(result);
+    }
+
+    @Test
+    void recordPatternsAllowed() {
+        ParseResult<Statement> result = javaParser.parse(STATEMENT, provider("if (a instanceof Box(String s)) {}"));
+        TestUtils.assertNoProblems(result);
     }
 
 }
