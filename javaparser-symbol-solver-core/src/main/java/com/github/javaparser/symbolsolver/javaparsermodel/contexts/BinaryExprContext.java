0bc4bbc7952ccc2a9fb20933b64b5aa7d11eabff
Johannes Coetzee <jcoetzee@qwiet.ai>
2024-08-19 16:51:11 +0200

    Fix patterns exposed to children of and and or expressions

--- javaparser-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/javaparsermodel/contexts/BinaryExprContext.java
+++ javaparser-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/javaparsermodel/contexts/BinaryExprContext.java
@@ -27,9 +27,9 @@ import com.github.javaparser.ast.expr.TypePatternExpr;
 import com.github.javaparser.resolution.Context;
 import com.github.javaparser.resolution.TypeSolver;
 import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFactory;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import com.github.javaparser.symbolsolver.javaparsermodel.PatternVariableResult;
+import com.github.javaparser.symbolsolver.javaparsermodel.PatternVariableVisitor;
+import java.util.*;
 
 public class BinaryExprContext extends ExpressionContext<BinaryExpr> {
 
@@ -182,24 +182,61 @@ public class BinaryExprContext extends ExpressionContext<BinaryExpr> {
     }
 
     public List<TypePatternExpr> typePatternExprsExposedToChild(Node child) {
-        BinaryExpr binaryExpr = wrappedNode;
-        Expression leftBranch = binaryExpr.getLeft();
-        Expression rightBranch = binaryExpr.getRight();
-
-        List<TypePatternExpr> results = new ArrayList<>();
-        if (child == leftBranch) {
-            results.addAll(patternExprsExposedToDirectParentFromBranch(leftBranch));
-        } else if (child == rightBranch) {
-            if (binaryExpr.getOperator().equals(BinaryExpr.Operator.AND)) {
-                // "" instanceof String s && "" instanceof String s2
-                results.addAll(patternExprsExposedToDirectParentFromBranch(leftBranch));
-            }
+        if (wrappedNode.getOperator().equals(BinaryExpr.Operator.AND)) {
+            return typePatternExprsExposedToChildByAnd(child);
         }
-        //        else if (binaryExpr.getOperator().equals(BinaryExpr.Operator.AND) && rightBranch.isAncestorOf(child))
-        // {
-        //            // "" instanceof String s && "" instanceof String s2
-        //            results.addAll(patternExprsExposedToDirectParentFromBranch(leftBranch));
-        //        }
+        if (wrappedNode.getOperator().equals(BinaryExpr.Operator.OR)) {
+            return typePatternExprsExposedToChildByOr(child);
+        }
+        return Collections.emptyList();
+    }
+
+    /**
+     * The following rules apply to a conditional-and expression a && b:
+     * - A pattern variable introduced by a when true is definitely matched at b.
+     *
+     * https://docs.oracle.com/javase/specs/jls/se21/html/jls-6.html#jls-6.3.1.1
+     */
+    private List<TypePatternExpr> typePatternExprsExposedToChildByAnd(Node child) {
+        if (!wrappedNode.getOperator().equals(BinaryExpr.Operator.AND)) {
+            throw new IllegalStateException(
+                    "Attempting to find patterns exposed by &&-expression, but wrapped operator is a "
+                            + wrappedNode.getOperator().asString());
+        }
+
+        List<TypePatternExpr> results = new LinkedList<>();
+
+        if (wrappedNode.getRight().containsWithinRange(child)) {
+            PatternVariableVisitor variableVisitor = PatternVariableVisitor.getInstance();
+            PatternVariableResult patternsInScope = wrappedNode.getLeft().accept(variableVisitor, null);
+
+            results.addAll(patternsInScope.getVariablesIntroducedIfTrue());
+        }
+
+        return results;
+    }
+
+    /**
+     * The following rules apply to a conditional-and expression a || b:
+     * - A pattern variable introduced by a when false is definitely matched at b.
+     *
+     * https://docs.oracle.com/javase/specs/jls/se21/html/jls-6.html#jls-6.3.1.2
+     */
+    private List<TypePatternExpr> typePatternExprsExposedToChildByOr(Node child) {
+        if (!wrappedNode.getOperator().equals(BinaryExpr.Operator.OR)) {
+            throw new IllegalStateException(
+                    "Attempting to find patterns exposed by ||-expression, but wrapped operator is a "
+                            + wrappedNode.getOperator().asString());
+        }
+
+        List<TypePatternExpr> results = new LinkedList<>();
+
+        if (wrappedNode.getRight().containsWithinRange(child)) {
+            PatternVariableVisitor variableVisitor = PatternVariableVisitor.getInstance();
+            PatternVariableResult patternsInScope = wrappedNode.getLeft().accept(variableVisitor, null);
+
+            results.addAll(patternsInScope.getVariablesIntroducedIfFalse());
+        }
 
         return results;
     }
