f7e1e26ee0858bda1df4c91f8aa8140e10d7f26e
Kimmmey <gravedigger1000@hotmail.com>
2024-09-30 17:49:23 +0200

    spotless apply

--- javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/ResolutionOfOverloadedMethodsWithTypeVariablesInvolvedTest.java
+++ javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/ResolutionOfOverloadedMethodsWithTypeVariablesInvolvedTest.java
@@ -1,40 +1,38 @@
 package com.github.javaparser.symbolsolver;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
-import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class ResolutionOfOverloadedMethodsWithTypeVariablesInvolvedTest {
 
     @Test
     public void test() {
-        String code = "public class Box<E> {\n" +
-                      "    private E element;\n" +
-                      "\n" +
-                      "    public Box(E element) {\n" +
-                      "        this.element = element;\n" +
-                      "    }\n" +
-                      "\n" +
-                      "    public E getElement() {return element;}\n" +
-                      "\n" +
-                      "    public void setElement(E element) {this.element = element;}\n" +
-                      "\n" +
-                      "    @Override\n" +
-                      "    public String toString() {\n" +
-                      "        StringBuilder builder = new StringBuilder();\n" +
-                      "        builder.append(\"Box [element=\");\n" +
-                      "        builder.append(element == this ? \"(this box)\" : element);\n" +
-                      "        builder.append(\"]\");\n" +
-                      "        return builder.toString();\n" +
-                      "    }\n" +
-                      "}";
+        String code = "public class Box<E> {\n" + "    private E element;\n"
+                + "\n"
+                + "    public Box(E element) {\n"
+                + "        this.element = element;\n"
+                + "    }\n"
+                + "\n"
+                + "    public E getElement() {return element;}\n"
+                + "\n"
+                + "    public void setElement(E element) {this.element = element;}\n"
+                + "\n"
+                + "    @Override\n"
+                + "    public String toString() {\n"
+                + "        StringBuilder builder = new StringBuilder();\n"
+                + "        builder.append(\"Box [element=\");\n"
+                + "        builder.append(element == this ? \"(this box)\" : element);\n"
+                + "        builder.append(\"]\");\n"
+                + "        return builder.toString();\n"
+                + "    }\n"
+                + "}";
         StaticJavaParser.getParserConfiguration().setSymbolResolver(new JavaSymbolSolver(new ReflectionTypeSolver()));
         CompilationUnit cu = StaticJavaParser.parse(code);
 
@@ -42,11 +40,14 @@ public class ResolutionOfOverloadedMethodsWithTypeVariablesInvolvedTest {
 
         List<String> expected = new ArrayList<>();
         expected.add("builder.append(\"Box [element=\")");
-        expected.add("ReflectionMethodDeclaration{method=public java.lang.StringBuilder java.lang.StringBuilder.append(java.lang.String)}");
+        expected.add(
+                "ReflectionMethodDeclaration{method=public java.lang.StringBuilder java.lang.StringBuilder.append(java.lang.String)}");
         expected.add("builder.append(element == this ? \"(this box)\" : element)");
-        expected.add("ReflectionMethodDeclaration{method=public java.lang.StringBuilder java.lang.StringBuilder.append(java.lang.Object)}");
+        expected.add(
+                "ReflectionMethodDeclaration{method=public java.lang.StringBuilder java.lang.StringBuilder.append(java.lang.Object)}");
         expected.add("builder.append(\"]\")");
-        expected.add("ReflectionMethodDeclaration{method=public java.lang.StringBuilder java.lang.StringBuilder.append(java.lang.String)}");
+        expected.add(
+                "ReflectionMethodDeclaration{method=public java.lang.StringBuilder java.lang.StringBuilder.append(java.lang.String)}");
         expected.add("builder.toString()");
         expected.add("ReflectionMethodDeclaration{method=public java.lang.String java.lang.StringBuilder.toString()}");
 
