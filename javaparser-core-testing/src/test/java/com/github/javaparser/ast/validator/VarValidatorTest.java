602f8edcf651b3ea6a08e1e400fc9922018b16d1
jlerbsc <jean-pierre.lerbscher@jperf.com>
2024-10-10 15:18:11 +0200

    Fix: issue 4579 Switch expr and var incompatibility

---
+++ javaparser-core-testing/src/test/java/com/github/javaparser/ast/validator/VarValidatorTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2007-2010 Júlio Vilmar Gesser.
+ * Copyright (C) 2011, 2013-2024 The JavaParser Team.
+ *
+ * This file is part of JavaParser.
+ *
+ * JavaParser can be used either under the terms of
+ * a) the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * b) the terms of the Apache License
+ *
+ * You should have received a copy of both licenses in LICENCE.LGPL and
+ * LICENCE.APACHE. Please refer to those files for details.
+ *
+ * JavaParser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ */
+
+package com.github.javaparser.ast.validator;
+
+import static com.github.javaparser.ParseStart.STATEMENT;
+import static com.github.javaparser.Providers.provider;
+import static com.github.javaparser.utils.TestUtils.assertNoProblems;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ParserConfiguration;
+import com.github.javaparser.ParserConfiguration.LanguageLevel;
+import com.github.javaparser.ast.stmt.Statement;
+import org.junit.jupiter.api.Test;
+
+class VarValidatorTest {
+    public static final JavaParser javaParser =
+            new JavaParser(new ParserConfiguration().setLanguageLevel(LanguageLevel.BLEEDING_EDGE));
+
+    @Test
+    void typePatternExpr() {
+        ParseResult<Statement> result = javaParser.parse(
+                STATEMENT, provider("switch (shape) {\n" + "  case Circle(var radius) -> { } \n" + "}"));
+
+        assertNoProblems(result);
+    }
+}
