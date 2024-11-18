d2402ff0a79118cac8e421e9eb3608b196a26ad4
Laugh <2636697181@qq.com>
2024-11-18 17:15:52 +0800

    Fix format error

--- javaparser-core/src/main/java/com/github/javaparser/utils/VisitorSet.java
+++ javaparser-core/src/main/java/com/github/javaparser/utils/VisitorSet.java
@@ -149,9 +149,7 @@ public class VisitorSet<N extends Node> implements Set<N> {
 
     @Override
     public String toString() {
-        return innerSet.stream()
-                .map(facade -> facade.overridden.toString())
-                .collect(Collectors.joining(",", "[", "]"));
+        return innerSet.stream().map(facade -> facade.overridden.toString()).collect(Collectors.joining(",", "[", "]"));
     }
 
     private class EqualsHashcodeOverridingFacade implements Visitable {
