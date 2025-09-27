0129860ddb2dcd88dfae0d3fd8dd30d5f8d84b45
PiTheGuy <the4mooresrock@gmail.com>
2025-09-27 15:13:21 -0500

    Spotless

--- javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/Issue4846Test.java
+++ javaparser-symbol-solver-testing/src/test/java/com/github/javaparser/symbolsolver/Issue4846Test.java
@@ -1,5 +1,7 @@
 package com.github.javaparser.symbolsolver;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.ast.CompilationUnit;
@@ -8,13 +10,10 @@ import com.github.javaparser.ast.body.TypeDeclaration;
 import com.github.javaparser.symbolsolver.resolution.AbstractResolutionTest;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
 import java.nio.file.Path;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class Issue4846Test extends AbstractResolutionTest {
     public static final Path SRC_DIR = adaptPath("src/test/resources/issue4846");
@@ -24,9 +23,7 @@ public class Issue4846Test extends AbstractResolutionTest {
     @BeforeEach
     void setup() {
         ParserConfiguration configuration = new ParserConfiguration()
-                .setSymbolResolver(new JavaSymbolSolver(new CombinedTypeSolver(
-                        new JavaParserTypeSolver(SRC_DIR)
-                )))
+                .setSymbolResolver(new JavaSymbolSolver(new CombinedTypeSolver(new JavaParserTypeSolver(SRC_DIR))))
                 .setLanguageLevel(ParserConfiguration.LanguageLevel.JAVA_16);
 
         javaParser = new JavaParser(configuration);
@@ -34,7 +31,10 @@ public class Issue4846Test extends AbstractResolutionTest {
 
     @Test
     void test() throws IOException {
-        CompilationUnit cu = javaParser.parse(SRC_DIR.resolve("foo").resolve("Main.java")).getResult().get();
+        CompilationUnit cu = javaParser
+                .parse(SRC_DIR.resolve("foo").resolve("Main.java"))
+                .getResult()
+                .get();
         TypeDeclaration<?> typeDec = cu.getType(0);
         MethodDeclaration methodDec = typeDec.getMethodsByName("foo").get(0);
         assertDoesNotThrow(methodDec::toDescriptor);
