30eaccbfdc5d1ef100bb5142c71b1eb791a6e4ee
Johannes Coetzee <jcoetzee@protonmail.com>
2025-12-01 18:09:59 +0100

    Add AST tests for module imports

--- javaparser-core-testing/src/test/java/com/github/javaparser/ast/imports/ImportDeclarationTest.java
+++ javaparser-core-testing/src/test/java/com/github/javaparser/ast/imports/ImportDeclarationTest.java
@@ -23,11 +23,21 @@ package com.github.javaparser.ast.imports;
 
 import static com.github.javaparser.StaticJavaParser.parseImport;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.github.javaparser.ParserConfiguration;
+import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.ImportDeclaration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class ImportDeclarationTest {
+
+    @BeforeAll
+    static void initParser() {
+        StaticJavaParser.getParserConfiguration().setLanguageLevel(ParserConfiguration.LanguageLevel.JAVA_25);
+    }
+
     @Test
     void singleTypeImportDeclaration() {
         ImportDeclaration i = parseImport("import a.b.c.X;");
@@ -52,5 +62,12 @@ class ImportDeclarationTest {
     void staticImportOnDemandDeclaration() {
         ImportDeclaration i = parseImport("import static a.b.c.X.*;");
         assertEquals("a.b.c.X", i.getNameAsString());
+    }
+
+    @Test
+    void moduleImport() {
+        ImportDeclaration i = parseImport("import module java.base;");
+        assertEquals("java.base", i.getNameAsString());
+        assertTrue(i.isModule());
     }
 }
