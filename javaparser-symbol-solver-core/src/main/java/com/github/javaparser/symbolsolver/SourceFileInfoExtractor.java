3fb3c394a48ff57d457f14377ab01a0a18d2fb39
jlerbsc <jean-pierre.lerbscher@jperf.com>
2025-12-20 23:02:27 +0100

    Fix: add the symbol resolver declaration in the parser configuration

--- javaparser-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/SourceFileInfoExtractor.java
+++ javaparser-symbol-solver-core/src/main/java/com/github/javaparser/symbolsolver/SourceFileInfoExtractor.java
@@ -25,6 +25,7 @@ import static com.github.javaparser.StaticJavaParser.parse;
 import static com.github.javaparser.resolution.Navigator.demandParentNode;
 import static java.util.Comparator.comparing;
 
+import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
 import com.github.javaparser.ast.Node;
@@ -43,6 +44,7 @@ import com.github.javaparser.resolution.model.SymbolReference;
 import com.github.javaparser.resolution.types.ResolvedReferenceType;
 import com.github.javaparser.resolution.types.ResolvedType;
 import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
+
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.FileVisitResult;
@@ -73,6 +75,8 @@ public class SourceFileInfoExtractor {
 
     public SourceFileInfoExtractor(TypeSolver typeSolver) {
         this.typeSolver = typeSolver;
+        // We must initialise the symbol resolver in the parser configuration in order to resolve expressions.
+        StaticJavaParser.getParserConfiguration().setSymbolResolver(new JavaSymbolSolver(typeSolver));
     }
 
     public void setVerbose(boolean verbose) {
